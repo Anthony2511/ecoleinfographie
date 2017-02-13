97d311d23673cedbca0f382ade0a827f61a635a3
Jimmy <jimmy@letecheur.me>
2017-02-13 11:42:00 +0100

    test langueFileManager and add options in menu

---
+++ ecoleinfographie/resources/lang/fr/auth.php
@@ -0,0 +1,6 @@
+<?php 
+
+ return [
+    "failed" => "Ces identifiants ne sont pas disponible dans notre base de donnée.",
+    "throttle" => "Too many login attempts. Please try again in :seconds seconds."
+];
--- ecoleinfographie/resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ ecoleinfographie/resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -30,17 +30,12 @@
           </li>
 
           <!-- Item LangFileManager -->
-          <li>
-            <a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/language') }}">
-              <i class="fa fa-flag-o"></i>
-              <span>Languages</span>
-            </a>
-          </li>
-          <li>
-            <a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/language/texts') }}">
-              <i class="fa fa-language"></i>
-              <span>Gestionnaire de langues</span>
-            </a>
+          <li class="treeview">
+            <a href="#"><i class="fa fa-globe"></i> <span>Translations</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+              <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/language') }}"><i class="fa fa-flag-checkered"></i> Languages</a></li>
+              <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/language/texts') }}"><i class="fa fa-language"></i> Site texts</a></li>
+            </ul>
           </li>
 
 
