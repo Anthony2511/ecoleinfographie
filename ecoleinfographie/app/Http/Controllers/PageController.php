d300d05048ebe11090df7035e4c3abf8b33eeb84
Jimmy <jimmy@letecheur.me>
2017-02-13 18:22:04 +0100

    Create function for homepage access at mysite/

--- ecoleinfographie/app/Http/Controllers/PageController.php
+++ ecoleinfographie/app/Http/Controllers/PageController.php
@@ -6,6 +6,30 @@ use Backpack\PageManager\app\Models\Page;
 
 class PageController extends Controller
 {
+	/*public function home($slug){
+		$page = Page::findBySlug($slug);
+		
+		if ($slug = 'Accueil')
+		{
+			abort(404, 'Please go back to our <a href="'.url('').'">homepage</a>.');
+		}
+		return view('pages.'.$page->template, $this->data);
+	}*/
+	
+	public function home()
+	{
+		$page = Page::findBySlug('accueil');
+		if (!$page)
+		{
+			abort(404, 'Please go back to our <a href="'.url('').'">homepage</a>.');
+		}
+		
+		$this->data['title'] = $page->title;
+		$this->data['page'] = $page->withFakes();
+		
+		return view('pages.home', $this->data);
+	}
+	
 	public function index($slug)
 	{
 		$page = Page::findBySlug($slug);
