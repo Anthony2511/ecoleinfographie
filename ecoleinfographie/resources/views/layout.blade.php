fe78eb5b36c5ba1316700f06ae868807196efb73
Jimmy <jimmy@letecheur.me>
2017-02-15 23:02:43 +0100

    Structure for menu dropdown and accessible

--- ecoleinfographie/resources/views/layout.blade.php
+++ ecoleinfographie/resources/views/layout.blade.php
@@ -3,10 +3,19 @@
 @include('partials.head')
 <body class="<?php if($page){ echo $page->slug; } ;?>">
 
-@include('partials.header')
+<header class="header-shawl">
+  @include('partials.header')
+</header>
 
 <div class="container">
     @yield('content')
 </div>
+<script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous">
+</script>
+<script src="./js/jquery-accessibleMegaMenu.js"></script>
+<script src="./js/main.js"></script>
 </body>
 </html>
--- ecoleinfographie/resources/views/partials/header.blade.php
+++ ecoleinfographie/resources/views/partials/header.blade.php
@@ -1,3 +1,82 @@
-<header>
-
-</header>
+<nav>
+    <ul class="menu">
+        <li class="menu__item">
+            <a href="#1" class="menu__link">Les métiers</a>
+            <div class="subnav">
+                <ul class="subnav__group">
+                    <li>
+                        <a href="">Les métiers du web</a>
+                    </li>
+                    <li>
+                        <a href="">La formation</a>
+                    </li>
+                    <li>
+                        <a href="">Le programme</a>
+                    </li>
+                    <li>
+                        <a href="">Les anciens</a>
+                    </li>
+                </ul>
+                <ul class="subnav__group">
+                    <li>
+                        <a href="">Les métiers du design graphique</a>
+                    </li>
+                    <li>
+                        <a href="">La formation</a>
+                    </li>
+                    <li>
+                        <a href="">Le programme</a>
+                    </li>
+                    <li>
+                        <a href="">Les anciens</a>
+                    </li>
+                </ul>
+                <ul class="subnav__group">
+                    <li>
+                        <a href="">Les métiers de la 3D et de l’audiovisuel</a>
+                    </li>
+                    <li>
+                        <a href="">La formation</a>
+                    </li>
+                    <li>
+                        <a href="">Le programme</a>
+                    </li>
+                    <li>
+                        <a href="">Les anciens</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="menu__item">
+            <a href="#2">L’école</a>
+            <div class="subnav">
+                <ul class="subnav__group">
+                    <li>
+                        <a href="">L’actualité</a>
+                    </li>
+                    <li>
+                        <a href="">Les commodités</a>
+                    </li>
+                    <li>
+                        <a href="">Le matériel et les batiments</a>
+                    </li>
+                    <li>
+                        <a href="">Contactez-nous</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="menu__item">
+            <a href="#3" class="menu__link">Réalisations</a>
+        </li>
+        <li class="menu__item">
+            <a href="#4" class="menu__link">Nos diplômés</a>
+        </li>
+        <li class="menu__item" >
+            <a href="#5" class="menu__link">Blog</a>
+        </li>
+        <li class="menu__item">
+            <a href="#6" class="menu__link">Je veux m’inscrire</a>
+        </li>
+    </ul>
+</nav>
