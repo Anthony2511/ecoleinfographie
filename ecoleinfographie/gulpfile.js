5799ed969e66299a5bfee65bc1b5c05c53524663
Jimmy <jimmy@letecheur.me>
2017-02-17 22:26:47 +0100

    update resources and init grid system

--- ecoleinfographie/gulpfile.js
+++ ecoleinfographie/gulpfile.js
@@ -1,14 +1,21 @@
 var elixir = require('laravel-elixir'),
     gulp = require('gulp'),
     stylus = require('laravel-elixir-stylus'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    tinypng = require('elixir-tinypng');
 
 
 
 elixir(function(mix) {
+    mix.tinypng({
+        key: '6R3ddSeN039Qn91I3YBCUN2j7XjAV-7u',
+        sigFile: '.tinypng-sigs',
+        summarize: true,
+    });
     mix.stylus('app.styl'), null, {
         use: [autoprefixer('last 7 versions', 'ie8' )]
     };
+
     mix.browserSync({
         proxy: 'ecoleinfographie.app'
     });
