1802eedf5c10432dc4afea21c01ecb23f0091312
Jimmy <jimmy@letecheur.me>
2017-02-18 15:37:28 +0100

    animation metier card menu

--- ecoleinfographie/resources/views/partials/header.blade.php
+++ ecoleinfographie/resources/views/partials/header.blade.php
@@ -72,6 +72,15 @@
                             </li>
                         </div>
                     </ul>
+                    <svg class="subnav__jobs__cloud" width="58" height="32" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M46.6 11c-1.5 0-3 .2-4.2.7v-.5c0-6.2-5-11.2-11-11.2-5.7 0-10.3 4.2-11 9.7l-1.7-.2c-2.7 0-5 1.5-6.2 3.7-.7-.2-1.5-.3-2.4-.3-5.3 0-10 4-10 9.3C0 27.7 4.6 32 10 32h38c.3 0 .6 0 .8-.2 5-1 9-5.3 9-10.3 0-6-5-10.6-11.2-10.6z" fill="#F5F5FA"/>
+                    </svg>
+                    <div class="subnav__jobs__town">
+                        <svg width="651" height="154" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M-76 168.4v-45.2h4.5v-13.4h27.3V98.4h17v13h9.3v-13h10V70h5v-6.4H8V70h4v25h4v21h8.8V78h6V48.7h4v-19h3V15.5h3V0H45v15h3.5v11.8h3v23h4v29.7H60v42.7h7.6v-10h16v5h5.8V82.5H94V70h13.8v12l3 3v20.3h5.4V99h14v6.8h8.4v6.5h9v-9h6.4v-19h36.3v19h6.4v-19h30.8V92h8.5v28h12V96h40v12.8h6v16.4h4v10h17.4v-30h17v10h6v-10H358v18h4v12h9.4v-20.4H393v-5h4.4v-6h4.5v5h5v-6h11v5.5h5v16h9v-22h19v6.5h8V81.5l6-11v-12l3 12.5 7 10v43.2h8.2v-82h25.3v76h5v-13h11v8h22.7v10h18v-15.4h9.4v9h7v-5.5h10v-6.5h11.8v17h9v-33l6-7.8 8.3 8.4v15h10v13.3h12v-15h8.2v-6.3h12v4.4h8v-7h29.3v23.4l2.5-5h11v28.4h8.2V131h3.4v-29.7H743v42.3h6v8.4h12v-7h25V98h19.4v48H845v24.4" fill="#F5F5FA" fill-rule="evenodd"/>
+                        </svg>
+                    </div>
+
                 </div>
             </li>
             <li class="menu__item">
