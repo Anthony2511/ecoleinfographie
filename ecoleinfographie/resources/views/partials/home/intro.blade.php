74eb7e53bdc735b5b4298ed5b4fba197731aff39
Jimmy <jimmy@letecheur.me>
2017-02-22 21:51:37 +0100

    reorganization of the files into partials

---
+++ ecoleinfographie/resources/views/partials/home/intro.blade.php
@@ -0,0 +1,20 @@
+<section class="intro">
+    <div class="intro__content">
+        <h2 aria-level="2" role="heading" class="intro__title">Tu veux devenir un infographiste&nbsp;?</h2>
+        <p class="intro__text">
+            In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place for at one and the same time.
+        </p>
+        <ul class="intro__list">
+            <li class="intro__item">
+                <a href="#" class="intro__link">La formation en web</a>
+            </li>
+            <li class="intro__item">
+                <a href="#" class="intro__link">La formation en design graphique</a>
+            </li>
+            <li class="intro__item">
+                <a href="#" class="intro__link">La formation en 3D et audiovisuel</a>
+            </li>
+        </ul>
+    </div>
+    <img class="intro__img" src="../img/image-infographiste.jpg" width="570" height="638" alt="Une homme à lunettes assis sur son bureau qui est entrain de lire une feuille qu’il tient dans sa main gauche en étant face à son ordinateur">
+</section>
---
+++ ecoleinfographie/resources/views/partials/home/news-home.blade.php
@@ -0,0 +1,8 @@
+<article class="news-home" role="article">
+    <img src="#" alt="#" class="article__img">
+    <h2 aria-level="2" role="heading" class="news-home__title">Saint-Nicolas est passé à l’école&nbsp;!</h2>
+    <p class="news-home__excerpt">
+        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500. Quand un peintre anonyme assembla ensemble des morceaux de…
+    </p>
+    <a href="#" class="article__link">Lire l’article <span>Saint-Nicolas est passé à l’école</span></a>
+</article>
