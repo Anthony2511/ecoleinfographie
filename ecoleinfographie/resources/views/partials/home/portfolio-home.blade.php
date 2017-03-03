00b24036a10b004a259bb47c2ae5aca3ccc642b2
Jimmy <jimmy@letecheur.me>
2017-03-03 22:23:29 +0100

    style final for portfolio-home

--- ecoleinfographie/resources/views/partials/home/portfolio-home.blade.php
+++ ecoleinfographie/resources/views/partials/home/portfolio-home.blade.php
@@ -9,55 +9,63 @@
             </p>
             <a href="#portfolio" class="portfolio-home__intro__link">Voir les travaux de nos étudiants</a>
         </div>
-        <div class="project-wrapper">
-            <article class="project project--small project--3D">
-                <figure class="project__figure">
-                    <img src="./img/portfolio-home__3D.jpg" srcset="../img/portfolio-home__3D@2x.jpg 2x" width="" height="" alt="" class="project__img">
-                </figure>
-                <div class="project__infos-wrapper">
-                    <div class="project__infos-container">
-                        <span class="project__category">Métiers de la 3D et de l’audiovisuel</span>
-                        <h3 role="heading" aria-level="3" class="project__title">La dame du lac</h3>
-                        <p class="project__description">Une modélisation de personnage réalisée durant le cours de 3D du bloc 3</p>
-                        <span class="project__author">Par Estelle Delvaux</span>
-                    </div>
+        <div class="project__container-all">
+            <div class="project__wrapper-top-inner">
+                <div class="project__wrapper-top">
+                    <article class="project project--small project--3D">
+                        <figure class="project__figure">
+                            <img src="./img/portfolio-home__3D.jpg" srcset="../img/portfolio-home__3D@2x.jpg 2x" width="" height="" alt="" class="project__img">
+                        </figure>
+                        <div class="project__infos-wrapper">
+                            <div class="project__infos-container">
+                                <span class="project__category">Métiers de la 3D et de l’audiovisuel</span>
+                                <h3 role="heading" aria-level="3" class="project__title">La dame du lac</h3>
+                                <p class="project__description">Une modélisation de personnage réalisée durant le cours de 3D du bloc 3</p>
+                                <span class="project__author">Par Estelle Delvaux</span>
+                            </div>
+                        </div>
+                        <a href="#project" class="project__link">
+                            <span>Voir la fiche de présentation La dame du lac</span>
+                        </a>
+                    </article>
                 </div>
-                <a href="#project" class="project__link">
-                    <span>Voir la fiche de présentation La dame du lac</span>
-                </a>
-            </article>
-            <article class="project project--big project--web">
-                <figure class="project__figure">
-                    <img src="./img/portfolio-home__web.jpg" srcset="../img/portfolio-home__web@2x.jpg 2x" width="" height="" alt="" class="project__img">
-                </figure>
-                <div class="project__infos-wrapper">
-                    <div class="project__infos-container">
-                        <span class="project__category">Métiers du web</span>
-                        <h3 class="project__title">Sur un Baobab</h3>
-                        <p class="project__description">Un site web réalisé pour une A.S.B.L durant le cours de Design Web du bloc 2</p>
-                        <span class="project__author">Par Jimmy Letecheur</span>
+            </div>
+            <div class="project__wrapper">
+                <article class="project project--middle project--2D">
+                    <figure class="project__figure">
+                        <img src="./img/portfolio-home__2D.jpg" srcset="../img/portfolio-home__2D@2x.jpg 2x" width="566" height="365" alt="" class="project__img">
+                    </figure>
+                    <div class="project__infos-wrapper">
+                        <div class="project__infos-container">
+                            <span class="project__category">Métiers du design graphique</span>
+                            <h3 role="heading" aria-level="3" class="project__title">L’homme aux milles couleurs</h3>
+                            <p class="project__description">Une illustration réalisée dans le cadre du cours de créations graphiques du bloc 3</p>
+                            <span class="project__author">Par Henry Demoulin</span>
+                        </div>
                     </div>
-                </div>
-                <a href="#project" class="project__link">
-                    <span>Voir la fiche de présentation de Sur un Baobab</span>
-                </a>
-            </article>
-            <article class="project project--middle project--2D">
-                <figure class="project__figure">
-                    <img src="./img/portfolio-home__2D.jpg" srcset="../img/portfolio-home__2D@2x.jpg 2x" width="566" height="365" alt="" class="project__img">
-                </figure>
-                <div class="project__infos-wrapper">
-                    <div class="project__infos-container">
-                        <span class="project__category">Métiers du design graphique</span>
-                        <h3 role="heading" aria-level="3" class="project__title">L’homme aux milles couleurs</h3>
-                        <p class="project__description">Une illustration réalisée dans le cadre du cours de créations graphiques du bloc 3</p>
-                        <span class="project__author">Par Henry Demoulin</span>
+                    <a href="#project" class="project__link">
+                        <span>Voir la fiche de présentation de L’homme aux milles couleurs</span>
+                    </a>
+                </article>
+                <article class="project project--big project--web">
+                    <figure class="project__figure">
+                        <img src="./img/portfolio-home__web.jpg" srcset="../img/portfolio-home__web@2x.jpg 2x" width="" height="" alt="" class="project__img">
+                    </figure>
+                    <div class="project__infos-wrapper">
+                        <div class="project__infos-container">
+                            <span class="project__category">Métiers du web</span>
+                            <h3 class="project__title">Sur un Baobab</h3>
+                            <p class="project__description">Un site web réalisé pour une A.S.B.L durant le cours de Design Web du bloc 2</p>
+                            <span class="project__author">Par Jimmy Letecheur</span>
+                        </div>
                     </div>
-                </div>
-                <a href="#project" class="project__link">
-                    <span>Voir la fiche de présentation de L’homme aux milles couleurs</span>
-                </a>
-            </article>
+                    <a href="#project" class="project__link">
+                        <span>Voir la fiche de présentation de Sur un Baobab</span>
+                    </a>
+                </article>
+            </div>
         </div>
     </section>
 </div>
+
+
