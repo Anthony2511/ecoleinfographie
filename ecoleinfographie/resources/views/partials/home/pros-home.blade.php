4c26633fbaf2b9cd0cb52e8aecec2abafa5f84db
Jimmy <jimmy@letecheur.me>
2017-03-05 15:01:28 +0100

    style base for pros-home slider

--- ecoleinfographie/resources/views/partials/home/pros-home.blade.php
+++ ecoleinfographie/resources/views/partials/home/pros-home.blade.php
@@ -7,4 +7,22 @@
             Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu.
         </p>
     </div>
+    <div class="slider-pros__wrapper">
+        {{--<div class="slider-pros__bg"></div>--}}
+        <article class="slider-pros__article" id="slider-pros__article01">
+            <div class="slider-pros__description">
+                <div class="slider-pros__description__inside">
+                    <h3 role="heading" aria-level="3" class="slider-pros__description__title">Toon Van den Bos</h3>
+                    <span class="slider-pros__description__subtitle">Co-fondateur de
+                        <a href="#" rel="ext">WhiteCube</a>
+                    </span>
+                    <p class="slider-pros__description__excerpt">In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volutpat aliquam. Integer et elit eget elit facilisis tristique. Nam vel iaculis mauris. Sed ullamcorper tellus erat. Eletium distoc etan rafiney logitec nono darvers.</p>
+                </div>
+                <a class="slider-pros__description__link" href="#">En savoir plus sur Toon</a>
+            </div>
+            <figure class="slider-pros__image">
+                <img src="./img/slider-pros__img1.jpg" width="338" height="359" alt="#">
+            </figure>
+        </article>
+    </div>
 </section>
