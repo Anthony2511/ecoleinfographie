29e8487cbf8f3036406e8c6704dbac9aafb56f32
Jimmy <jimmy@letecheur.me>
2017-03-14 23:21:09 +0100

    styles section web-what

---
+++ ecoleinfographie/resources/views/partials/web_home/content.blade.php
@@ -0,0 +1,16 @@
+<section class="web-what">
+    <div class="web-what__wrapper">
+        <img class="web-what__img" src="./img/macbook-web-what.png" width="764" height="879" alt="" data-type="parallax"
+             data-depth="0">
+        <div class="web-what__wrapper-text">
+            <h2 role="heading" aria-level="2" class="web-what__title">
+                Le web, c’est quoi <span>?</span>
+                {!! file_get_contents(asset('svg/question1.svg')) !!}
+            </h2>
+            <p class="web-what__text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet. Sed sit amet arcu aliquet, molestie justo at, auctor nunc. Phasellus ligula ipsum, volutpat eget semper id, viverra eget nibh.
+            </p>
+        </div>
+    </div>
+
+</section>
