b140d1383e241a678eb4103a057fa9f076db9528
Jimmy <jimmy@letecheur.me>
2017-03-15 17:28:08 +0100

    code & style section webdesigner

--- ecoleinfographie/resources/views/partials/web_home/content.blade.php
+++ ecoleinfographie/resources/views/partials/web_home/content.blade.php
@@ -1,7 +1,7 @@
 <section class="web-what">
     <div class="web-what__wrapper">
         <img class="web-what__img" src="./img/macbook-web-what.png" width="764" height="879" alt="" data-type="parallax"
-             data-depth="0">
+             data-depth="-0.07">
         <div class="web-what__wrapper-text">
             <h2 role="heading" aria-level="2" class="web-what__title">
                 Le web, c’est quoi <span>?</span>
@@ -12,5 +12,21 @@
             </p>
         </div>
     </div>
-
 </section>
+
+<section class="webdesigner">
+    <div class="webdesigner__wrapper">
+        <div class="webdesigner__wrapper-text">
+            <h2 role="heading" aria-level="2" class="webdesigner__title">
+                Le designer web
+            </h2>
+            <p class="webdesigner__text">
+                Il conçoit les interfaces et les manières de s’en servir. Ses outils sont plutôt des outils de graphistes que des outils de codeurs, mais il doit connaître le code et les contraintes techniques des navigateurs Web.
+            </p>
+            <a href="#webdesigner" class="webdesigner__link">Voir un ancien diplômé qui est designer web</a>
+        </div>
+        <img class="webdesigner__img" src="./img/web-designerweb.png" srcset="./img/web-designerweb@2x.png 2x" width="749" height="646" alt="">
+    </div>
+</section>
+
+
