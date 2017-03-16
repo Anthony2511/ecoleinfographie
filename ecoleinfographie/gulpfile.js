b412938a0e0ff86ed3b4395bb84984246fd11960
Jimmy <jimmy@letecheur.me>
2017-03-16 19:01:01 +0100

    update gulpfile

--- ecoleinfographie/gulpfile.js
+++ ecoleinfographie/gulpfile.js
@@ -13,15 +13,15 @@ elixir(function (mix) {
         summarize: true,
     });
 
-    mix.copy('resources/assets/img/*.*', 'public/img/');
-    mix.copy('resources/assets/svg-inline/*.*', 'public/svg/');
-    mix.copy('resources/assets/fonts/*.*', 'public/fonts/');
-
     mix.stylus('app.styl'), {
         use: [autoprefixer('last 7 versions', 'ie8' )]
     };
 
     mix.scriptsIn();
+
+    //mix.copy('resources/assets/img/*.*', 'public/img/');
+    mix.copy('resources/assets/svg-inline/*.*', 'public/svg/');
+    mix.copy('resources/assets/fonts/*.*', 'public/fonts/');
 
     mix.browserSync({
         proxy: 'ecoleinfographie.app'
