4a48dad6cceac1df1a2c90f6aeb39f882ba44c16
Jimmy <jimmy@letecheur.me>
2017-03-23 18:46:24 +0100

    html formation-intro

---
+++ ecoleinfographie/resources/views/partials/web/intro-formation.blade.php
@@ -0,0 +1,50 @@
+<section class="formation-intro">
+    <h2 role="heading" aria-level="2" class="formation-intro__title">
+        <span>Découvre </span>
+        <strong>la formation en web</strong>
+    </h2>
+    <img src="./img/formation-web-intro.jpg" width="610" height="399" alt="Une personne de dos entrain d’écrire sur un ordinateur portable qui a un fond d’écran ou il est écrit webdesign" class="formation-intro__img">
+    <section class="formation-quality">
+        <div class="formation-quality__container-text">
+            <h3 role="heading" aria-level="3" class="formation-quality__title">
+                <span>Avant tout </span>
+                <strong>un web de qualité</strong>
+            </h3>
+            <p class="formation-quality__text">
+                Dans la formation Web, les profs sont passionnés par la qualité. Ça peut vouloir dire tout et rien à la fois qualité. Dans le Web, ça veut dire qu’on essaie de faire des sites ou des applications tous-terrains, qui marchent dans un maximum de situations.
+            </p>
+            <a href="#web-course" class="formation-quality__link">
+                <span>En savoir plus sur les cours de l’option web</span>
+            </a>
+        </div>
+        <div class="skills-web__container">
+            <span class="skills-web__title">Des produits qui réponde à des critères tel que&nbsp;:</span>
+            <ul class="skills-web__list">
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Performants</span>
+                    <p class="skills-web__item__text">Un produit qui ne nécessitent pas d’avoir des machines ou des connexions à la pointe.</p>
+                </li>
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Éfficaces</span>
+                    <p class="skills-web__item__text">Qui répondent à leur cahier de charge avec succès.</p>
+                </li>
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Agréables</span>
+                    <p class="skills-web__item__text">Qui donnent satisfaction à leurs utilisateurs et ne leur prennent pas la tête.</p>
+                </li>
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Écologiques</span>
+                    <p class="skills-web__item__text">Parce que oui, le web a un impact écologique énorme.</p>
+                </li>
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Sûrs</span>
+                    <p class="skills-web__item__text">Qui réduisent les risques d’erreurs.</p>
+                </li>
+                <li class="skills-web__item">
+                    <span class="skills-web__item__title">Extensibles</span>
+                    <p class="skills-web__item__text">Qui sont maintenable et qui peuvent évoluer dans le temps sans contraintes.</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+</section>
