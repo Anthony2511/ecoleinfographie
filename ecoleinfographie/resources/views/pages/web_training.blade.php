cc25549e23f20360e5afdc5b5f4efe76e55af269
Jimmy <jimmy@letecheur.me>
2017-03-26 16:12:38 +0200

    style formation bloc info & intro

--- ecoleinfographie/resources/views/pages/web_training.blade.php
+++ ecoleinfographie/resources/views/pages/web_training.blade.php
@@ -7,5 +7,6 @@
 @section('content')
     @include('partials.breadcrumb')
     @include('partials.web.intro-formation')
+    @include('partials.web.formation-bloc-1')
 @endsection
 
--- ecoleinfographie/resources/views/partials/web/intro-formation.blade.php
+++ ecoleinfographie/resources/views/partials/web/intro-formation.blade.php
@@ -1,62 +1,88 @@
 <section class="formation-intro">
-    <h2 role="heading" aria-level="2" class="formation-intro__title">
-        <span>Découvre </span>
-        <strong>la formation en web</strong>
-    </h2>
-    <section class="formation-quality">
-        <img src="./img/formation-web-intro.jpg" width="610" height="399" alt="Une personne de dos entrain d’écrire sur un ordinateur portable qui a un fond d’écran ou il est écrit webdesign" class="formation-intro__img">
-        <div class="formation-quality__intro-container">
-            <div class="formation-quality__container-text">
-                <h3 role="heading" aria-level="3" class="formation-quality__title">
-                    <span>Avant tout </span>
-                    <strong>un web de qualité</strong>
-                </h3>
-                <p class="formation-quality__text">
-                    Dans la formation Web, les profs sont passionnés par la qualité. Ça peut vouloir dire tout et rien à la fois qualité. Dans le Web, ça veut dire qu’on essaie de faire des sites ou des applications tous-terrains, qui marchent dans un maximum de situations.
-                </p>
-                <div class="formation-quality__link-container">
-                    <a href="#web-course" class="formation-quality__link">
-                        <span>En savoir plus sur les cours de l’option web</span>
-                    </a>
+    <div class="formation-container-top">
+        <h2 role="heading" aria-level="2" class="formation-intro__title">
+            <span>Découvre </span>
+            <strong>la formation en web</strong>
+        </h2>
+        <section class="formation-quality">
+            <img src="./img/formation-web-intro.jpg" width="610" height="399" alt="Une personne de dos entrain d’écrire sur un ordinateur portable qui a un fond d’écran ou il est écrit webdesign" class="formation-intro__img">
+            <div class="formation-quality__intro-container">
+                <div class="formation-quality__container-text">
+                    <h3 role="heading" aria-level="3" class="formation-quality__title">
+                        <span>Avant tout </span>
+                        <strong>un web de qualité</strong>
+                    </h3>
+                    <p class="formation-quality__text">
+                        Dans la formation Web, les profs sont passionnés par la qualité. Ça peut vouloir dire tout et rien à la fois qualité. Dans le Web, ça veut dire qu’on essaie de faire des sites ou des applications tous-terrains, qui marchent dans un maximum de situations.
+                    </p>
+                    <div class="formation-quality__link-container">
+                        <a href="#web-course" class="formation-quality__link">
+                            <span>En savoir plus sur les cours de l’option web</span>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="skills-web">
-            <div class="skills-web__container">
-                <span class="skills-web__title">Des produits qui réponde à des critères tel que&nbsp;:</span>
-                <ul class="skills-web__list">
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/performance.svg')) !!}
-                        <span class="skills-web__item__title">Performants</span>
-                        <p class="skills-web__item__text">Un produit qui ne nécessitent pas d’avoir des machines ou des connexions à la pointe.</p>
-                    </li>
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/effective.svg')) !!}
-                        <span class="skills-web__item__title">Éfficaces</span>
-                        <p class="skills-web__item__text">Qui répondent à leur cahier de charge avec succès.</p>
-                    </li>
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/agreable.svg')) !!}
-                        <span class="skills-web__item__title">Agréables</span>
-                        <p class="skills-web__item__text">Qui donnent satisfaction à leurs utilisateurs et ne leur prennent pas la tête.</p>
-                    </li>
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/ecology.svg')) !!}
-                        <span class="skills-web__item__title">Écologiques</span>
-                        <p class="skills-web__item__text">Parce que oui, le web a un impact écologique énorme.</p>
-                    </li>
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/safe.svg')) !!}
-                        <span class="skills-web__item__title">Sûrs</span>
-                        <p class="skills-web__item__text">Qui réduisent les risques d’erreurs.</p>
-                    </li>
-                    <li class="skills-web__item">
-                        {!! file_get_contents(asset('svg/extensible.svg')) !!}
-                        <span class="skills-web__item__title">Extensibles</span>
-                        <p class="skills-web__item__text">Qui sont maintenable et qui peuvent évoluer dans le temps sans contraintes.</p>
-                    </li>
-                </ul>
+            <div class="skills-web">
+                <div class="skills-web__container">
+                    <span class="skills-web__title">Des produits qui réponde à des critères tel que&nbsp;:</span>
+                    <ul class="skills-web__list">
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/performance.svg')) !!}
+                            <span class="skills-web__item__title">Performants</span>
+                            <p class="skills-web__item__text">Un produit qui ne nécessitent pas d’avoir des machines ou des connexions à la pointe.</p>
+                        </li>
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/effective.svg')) !!}
+                            <span class="skills-web__item__title">Éfficaces</span>
+                            <p class="skills-web__item__text">Qui répondent à leur cahier de charge avec succès.</p>
+                        </li>
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/agreable.svg')) !!}
+                            <span class="skills-web__item__title">Agréables</span>
+                            <p class="skills-web__item__text">Qui donnent satisfaction à leurs utilisateurs et ne leur prennent pas la tête.</p>
+                        </li>
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/ecology.svg')) !!}
+                            <span class="skills-web__item__title">Écologiques</span>
+                            <p class="skills-web__item__text">Parce que oui, le web a un impact écologique énorme.</p>
+                        </li>
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/safe.svg')) !!}
+                            <span class="skills-web__item__title">Sûrs</span>
+                            <p class="skills-web__item__text">Qui réduisent les risques d’erreurs.</p>
+                        </li>
+                        <li class="skills-web__item">
+                            {!! file_get_contents(asset('svg/extensible.svg')) !!}
+                            <span class="skills-web__item__title">Extensibles</span>
+                            <p class="skills-web__item__text">Qui sont maintenable et qui peuvent évoluer dans le temps sans contraintes.</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
+        </section>
+    </div> <!-- end formation-container-top -->
+    <section class="formation-infos">
+        <div class="formation-infos__container-top">
+            <div class="formation-infos__title-wrap">
+                <h2 role="heading" aria-level="2" class="formation-infos__title">Une formation en deux étapes</h2>
+                <img src="./img/formation-web-infos-img1.jpg" width="421" height="381" alt="Deux étudiantes entrain de travailler sur un ordinateur" class="formation-infos__img1">
+                <img src="./img/formation-web-infos-img2.jpg" width="421" height="381" alt="Une photo tenue par une personne devant un ordinateur" class="formation-infos__img2">
+            </div>
+        </div>
+        <div class="formation-infos__text1">
+            <strong class="formation-infos__text1__title">Des débuts communs à tous</strong>
+            <p class="formation-infos__text1__text">Dans le bachelier en techniques graphiques, les débuts sont communs à tous les étudiants. C’est ce qu’on appelle le <a href="#bloc1">premier</a> bloc de cours. Ensuite, les étudiants peuvent se spécialiser selon leurs affinités en choisissant leurs cours dans le <a href="#bloc2">deuxième</a> et le <a href="#bloc3">troisième</a> bloc.
+            </p>
+        </div>
+    </section>
+    <section class="formation-choice">
+        <div class="formation-choice__wrapper">
+            <h2 role="heading" aria-level="2" class="formation-choice__title">
+                Dés le bloc 2, tu vas pouvoir choisir ton option et te spécialisé
+            </h2>
+            <p class="formation-choice__text">
+                Pour que la formation ait du sens, il faut choisir des cours qui vont ensemble et des cours pour lesquels on a acquis certaines compétences préalables. C’est ce qu’on appelle les <b>pré-requis </b>et les <b>co-requis</b>. Pas de quoi te casser la tête, le tout est très logique et tes profs pourront t’aider à construire un programme valable au besoin.
+            </p>
         </div>
     </section>
 </section>
