9057188689d062b4e9f31766c1ea8a4e2c1bb381
Jimmy <jimmy@letecheur.me>
2017-03-28 20:04:22 +0200

    style cards redirect to bloc formation

--- ecoleinfographie/resources/views/partials/web/intro-program.blade.php
+++ ecoleinfographie/resources/views/partials/web/intro-program.blade.php
@@ -1,36 +1,44 @@
 <section class="program">
-    <h2 role="heading" aria-level="2" class="program__title">
-        <span>Découvre </span>
-        <strong>la formation en web</strong>
-    </h2>
-    <div class="program__redirect">
-        <a href="#b1" class="program__redirect-link">
-            <figure class="program__redirect__figure">
-                <img class="program__redirect__img" src="./img/program-redirect--1.jpg" width="315" height="202" alt="Bureau vue de haut contenant des images en rapport avec l’infographie (smartphone, livre, brochures, crayons,…)">
-                <figcaption>
-                    <span>Le programme </span>
-                    <strong>du premier bloc</strong>
-                </figcaption>
-            </figure>
-        </a>
-        <a href="#b2" class="program__redirect-link">
-            <figure class="program__redirect__figure">
-                <img class="program__redirect__img" src="./img/program-redirect--2.jpg" width="315" height="202" alt="Un groupe de personne réunit autour d’une table qui contient des éléments en rapport avec l’infographie (smartphone, crayons, ordinateur,…)">
-                <figcaption>
-                    <span>Le programme </span>
-                    <strong>du deuxième bloc</strong>
-                </figcaption>
-            </figure>
-        </a>
-        <a href="#b3" class="program__redirect-link">
-            <figure class="program__redirect__figure">
-                <img class="program__redirect__img" src="./img/program-redirect--3.jpg" width="315" height="202" alt="Deux personnes travaillant ensemble sur un visuel sur un ordinateur">
-                <figcaption>
-                    <span>Le programme </span>
-                    <strong>du troisième bloc</strong>
-                </figcaption>
-            </figure>
-        </a>
+    <div class="program__container-top">
+        <h2 role="heading" aria-level="2" class="program__title">
+            <span>Découvre </span>
+            <strong>le programme des cours</strong>
+        </h2>
+        <div class="program__redirect">
+            <a href="#b1" class="program__redirect-link">
+                <figure class="program__redirect__figure">
+                    <div class="program__redirect__img-wrapper">
+                        <img class="program__redirect__img" src="./img/program-redirect--1.jpg" width="315" height="202" alt="Bureau vue de haut contenant des images en rapport avec l’infographie (smartphone, livre, brochures, crayons,…)">
+                    </div>
+                    <figcaption class="program__redirect__figcaption">
+                        <span>Le programme </span>
+                        <strong>du premier bloc</strong>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="#b2" class="program__redirect-link">
+                <figure class="program__redirect__figure">
+                    <div class="program__redirect__img-wrapper">
+                        <img class="program__redirect__img" src="./img/program-redirect--2.jpg" width="315" height="202" alt="Un groupe de personne réunit autour d’une table qui contient des éléments en rapport avec l’infographie (smartphone, crayons, ordinateur,…)">
+                    </div>
+                    <figcaption class="program__redirect__figcaption">
+                        <span>Le programme </span>
+                        <strong>du deuxième bloc</strong>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="#b3" class="program__redirect-link">
+                <figure class="program__redirect__figure">
+                    <div class="program__redirect__img-wrapper">
+                        <img class="program__redirect__img" src="./img/program-redirect--3.jpg" width="315" height="202" alt="Deux personnes travaillant ensemble sur un visuel sur un ordinateur">
+                    </div>
+                    <figcaption class="program__redirect__figcaption">
+                        <span>Le programme </span>
+                        <strong>du troisième bloc</strong>
+                    </figcaption>
+                </figure>
+            </a>
+        </div>
     </div>
     <section class="program__bloc program__bloc--1">
 
