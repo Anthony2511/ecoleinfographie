0efe9fdb2b1ed79d6e2bb1189c0a94ffb0705538
Jimmy <jimmy@letecheur.me>
2017-04-01 20:18:19 +0200

    play with lang file laravel

---
+++ ecoleinfographie/resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Votre mot de passe doit comporter au moins six caractères et correspondre à votre mot.',
+    'reset' => 'Votre mot de passe a été mis à jour.',
+    'sent' => 'Nous venons un email pour redéfinir votre mot de passe!',
+    'token' => 'Ce lien pour redéfinir votre mot de passe est.',
+    'user' => "Il n’y a aucun utilisateurs avec cette adresse email dans notre base de donnée.",
+
+];
---
+++ ecoleinfographie/resources/views/partials/web/former-students.blade.php
@@ -0,0 +1,1 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at debitis, dicta, eveniet facere iusto magnam modi mollitia odio quas quibusdam sit, unde? Beatae corporis praesentium quo rerum sint veritatis.</p>
