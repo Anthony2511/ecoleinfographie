9df9debb39c3d4faeaa25197423b0b79af57d128
Jimmy <jimmy@letecheur.me>
2017-04-09 21:28:45 +0200

    create grid for realisations and position

--- ecoleinfographie/resources/views/partials/realisations/index-realisations.blade.php
+++ ecoleinfographie/resources/views/partials/realisations/index-realisations.blade.php
@@ -22,12 +22,214 @@
                         <img class="reas__img" src="./img/reas-test.png" width="940" height="938" alt="">
                         <figcaption class="reas__figcaption">
                             <div class="reas__section">
-                                <span class="reas__section__name">Web</span>
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">Web</span>
+                                </span>
                             </div>
                             <div class="reas__desc">
                                 <div class="reas__desc-wrapper">
                                     <strong class="reas__desc__name">Sur un Baobab</strong>
                                     <span class="reas__desc__author">Par Jimmy Letecheur</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test2.png" width="1211" height="812" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">Design graphique</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Sur un Baobab</strong>
+                                    <span class="reas__desc__author">Par Jimmy Letecheur</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test3.png" width="671" height="662" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">Design graphique</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Sur un Baobab</strong>
+                                    <span class="reas__desc__author">Par Jimmy Letecheur</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test4.png" width="864" height="851" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">La voiture du Karting Jonhonsou</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test5.png" width="868" height="926" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">La voiture du Karting Jonhonsou</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test6.png" width="1299" height="950" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">La voiture du Karting Jonhonsou</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test7.png" width="805" height="770" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Lorem ipsum dolor sit amet aspilicuita</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test8.png" width="670" height="864" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Lorem ipsum dolor sit amet aspilicuita</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test9.png" width="525" height="345" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Lorem ipsum dolor sit amet aspilicuita</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test10.png" width="650" height="345" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Lorem ipsum dolor sit amet aspilicuita</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li class="reas__item">
+                <a href="#reas" class="reas__link">
+                    <figure class="reas__figure">
+                        <img class="reas__img" src="./img/reas-test11.png" width="899" height="616" alt="">
+                        <figcaption class="reas__figcaption">
+                            <div class="reas__section">
+                                <span class="reas__section__name">
+                                    <span class="reas__section__name-text">3D et audiovisuel</span>
+                                </span>
+                            </div>
+                            <div class="reas__desc">
+                                <div class="reas__desc-wrapper">
+                                    <strong class="reas__desc__name">Lorem ipsum dolor sit amet aspilicuita</strong>
+                                    <span class="reas__desc__author">Par Henri Dalum et Jessica Daufel</span>
                                 </div>
                             </div>
                         </figcaption>
