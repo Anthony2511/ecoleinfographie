9dea50b4da3d1bbb4b66958e21ae29c895885aa5
Jimmy <jimmy@letecheur.me>
2017-04-12 13:59:15 +0200

    create base page for home js

---
+++ ecoleinfographie/resources/assets/js/pages/home.js
@@ -0,0 +1,20 @@
+w.home = {
+      oConf: {},
+
+      init: function () {
+        this.getElements();
+        this.setEvents();
+        console.log('home charged');
+      },
+
+      getElements: function () {
+
+      },
+      setEvents: function () {
+
+      },
+
+      // EVENTS
+
+      // FUNCTIONS
+}
