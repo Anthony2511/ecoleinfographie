f1f85b63bae10245cc80fbc0f79ef5a5ba1f8218
Jimmy <jimmy@letecheur.me>
2017-04-12 11:50:33 +0200

    create front js base for import all files

---
+++ ecoleinfographie/resources/assets/js/front.js
@@ -0,0 +1,36 @@
+var w = {
+      // global vars
+      sCurrent: null,
+      isLoaded: false,
+
+      // global elements
+      $body: null,
+
+      // pages
+      home: {},
+
+      // setup
+      init: function () {
+            w.getElements();
+            w.sCurrent = w.getCurrentPage();
+            //w.mobileMenu.init();
+            w.loadPage();
+            w.isLoaded = true;
+      },
+      loadPage: function () {
+            if (w.sCurrent) w[w.sCurrent].init();
+      },
+
+      // functions
+      getElements: function () {
+            w.$body = $('body');
+      },
+
+      getCurrentPage: function () {
+            if (w.$body.hasClass('home')) return 'home';
+
+            return false;
+      }
+};
+
+$(window).on('load', w.init);
