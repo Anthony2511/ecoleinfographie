b17505501f7f76e007431a1286f3c10d3b538796
Jimmy <jimmy@letecheur.me>
2017-04-12 21:06:19 +0200

    create base for video/3D view on click and slide

--- ecoleinfographie/resources/assets/js/class/sliderRea.js
+++ ecoleinfographie/resources/assets/js/class/sliderRea.js
@@ -4,9 +4,25 @@ w.sliderRea = {
 
     init: function () {
         this.config();
+        this.runIframe();
     },
 
     config: function () {
         console.log('sliderRea charged');
+        $('.rSlider').lightSlider({
+            gallery: true,
+            item: 1,
+            slideMargin: 0,
+            onAfterSlide: function () {
+                $('.rSlider iframe').remove();
+                $('.rSlider li').removeClass('hasIframe');
+            }
+        });
+    },
+
+    runIframe: function () {
+        $('.rSlider__item--withIframe').on('click', function () {
+                $(this).addClass('hasIframe').append('<iframe width="945" height="531" src="' + $(this).attr('data-iframe') + '" frameborder="0" allowfullscreen></iframe>')
+        });
     }
 }
--- ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
+++ ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
@@ -1,23 +1,23 @@
 <div class="rea__container">
     <section class="rea">
         <h2 role="heading" aria-level="2" class="rea__name">Sur un Baobab</h2>
-        <span class="rea__author">Par <a href="#jimmyletecheur" class="rea__link" rel="author">Jimmy Letecheur</a></span>
+        <span class="rea__author">Par <a href="#jimmyletecheur" class="rea__link" rel="author" title="Voir les autre projets de Jimmy Letecheur">Jimmy Letecheur</a></span>
 
         <div class="rSlider__container">
             <ul class="rSlider">
-                <li class="rSlider__item" data-iframe="##">
+                <li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1">
                     <img class="rSlider__img" src="./img/rea-one.png" width="947" height="532" alt="#alt">
                 </li>
-                <li class="rSlider__item" data-iframe="##">
+                <li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1">
                     <img class="rSlider__img" src="./img/rea-two.png" width="947" height="532" alt="#alt">
                 </li>
-                <li class="rSlider__item" data-iframe="##">
+                <li class="rSlider__item">
                     <img class="rSlider__img" src="./img/rea-three.png" width="947" height="532" alt="#alt">
                 </li>
-                <li class="rSlider__item" data-iframe="##">
+                <li class="rSlider__item">
                     <img class="rSlider__img" src="./img/rea-four.png" width="947" height="532" alt="#alt">
                 </li>
-                <li class="rSlider__item" data-iframe="##">
+                <li class="rSlider__item">
                     <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
                 </li>
             </ul>
