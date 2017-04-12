9cdf0b29f13c94c9da984825e3fdc6f980b2810c
Jimmy <jimmy@letecheur.me>
2017-04-12 19:00:53 +0200

    create base template page for start oneRealisation

---
+++ ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
@@ -0,0 +1,6 @@
+<div class="rea__container">
+    <section class="rea">
+        <h2 role="heading" aria-level="2" class="rea__name">Sur un Baobab</h2>
+        <span class="rea__author">Par <a href="#jimmyletecheur" class="rea__link" rel="author">Jimmy Letecheur</a></span>
+    </section>
+</div>
