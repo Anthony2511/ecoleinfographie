88244849d07617a0e269f48cfa1046f145743054
Jimmy <jimmy@letecheur.me>
2017-04-13 21:47:59 +0200

    animate button plat

--- ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
+++ ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
@@ -7,10 +7,11 @@
             <ul class="rSlider">
                 <li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1" data-thumb="./img/rea-three.png">
                     <img class="rSlider__img" src="./img/rea-one.png" width="947" height="532" alt="#alt">
+                    {!! file_get_contents(public_path('svg/inline-play-button3D.svg')) !!}
                 </li>
                 <li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1" data-thumb="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" >
-                    <img class="rSlider__img" src="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg
-" width="947" height="532" alt="#alt">
+                    <img class="rSlider__img" src="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" width="947" height="532" alt="#alt">
+                    {!! file_get_contents(public_path('svg/inline-play-button.svg')) !!}
                 </li>
                 <li class="rSlider__item" data-thumb="./img/rea-three.png">
                     <img class="rSlider__img" src="./img/rea-three.png" width="947" height="532" alt="#alt">
