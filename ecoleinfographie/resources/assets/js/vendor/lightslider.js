0bcc0469cefd96694c5742fa687faf4645d932b1
Jimmy <jimmy@letecheur.me>
2017-04-13 14:41:04 +0200

    update link gallery to become button with label

--- ecoleinfographie/resources/assets/js/vendor/lightslider.js
+++ ecoleinfographie/resources/assets/js/vendor/lightslider.js
@@ -355,7 +355,7 @@
                         }
                         var thumb = $children.eq(i * settings.slideMove).attr('data-thumb');
                         if (settings.gallery === true) {
-                            pagers += '<li style="width:100%;' + property + ':' + thumbWidth + 'px;' + gutter + ':' + settings.thumbMargin + 'px"><a href="#"><img src="' + thumb + '" /></a></li>';
+                            pagers += '<li style="width:100%;' + property + ':' + thumbWidth + 'px;' + gutter + ':' + settings.thumbMargin + 'px"><button><span class="label-gallery">Faire défiler le slider à l’image"' + i+'"</span> <img src="' + thumb + '" /></button></li>';
                         } else {
                             pagers += '<li><a href="#">' + (i + 1) + '</a></li>';
                         }
