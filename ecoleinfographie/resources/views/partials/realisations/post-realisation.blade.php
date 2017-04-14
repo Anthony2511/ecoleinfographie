7ee45705bf50cf2c35b232f84222de57315cff50
Jimmy <jimmy@letecheur.me>
2017-04-14 13:09:06 +0200

    create structure for rea-infos

--- ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
+++ ecoleinfographie/resources/views/partials/realisations/post-realisation.blade.php
@@ -3,37 +3,71 @@
         <h2 role="heading" aria-level="2" class="rea__name">Sur un Baobab</h2>
         <span class="rea__author">Par <a href="#jimmyletecheur" class="rea__link" rel="author" title="Voir les autre projets de Jimmy Letecheur">Jimmy Letecheur</a></span>
 
-        <div class="rSlider__container">
-            <ul class="rSlider">
-                <li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1" data-thumb="./img/rea-3D.png">
-                    <img class="rSlider__img" src="./img/rea-3D.png" width="947" height="532" alt="#alt">
-                    {!! file_get_contents(public_path('svg/inline-play-button3D.svg')) !!}
-                </li>
-                <li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1" data-thumb="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" >
-                    <img class="rSlider__img" src="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" width="947" height="532" alt="#alt">
-                    {!! file_get_contents(public_path('svg/inline-play-button.svg')) !!}
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-three.png">
-                    <img class="rSlider__img" src="./img/rea-three.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-four.png">
-                    <img class="rSlider__img" src="./img/rea-four.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-five.png">
-                    <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-five.png">
-                    <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-five.png">
-                    <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb="./img/rea-five.png">
-                    <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-                </li>
-                <li class="rSlider__item" data-thumb=""></li>
-            </ul>
+        <div class="rea__wrapper-top">
+
+            <div class="rSlider__container">
+                <ul class="rSlider">
+                    <li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1" data-thumb="./img/rea-3D.png">
+                        <img class="rSlider__img" src="./img/rea-3D.png" width="947" height="532" alt="#alt">
+                        {!! file_get_contents(public_path('svg/inline-play-button3D.svg')) !!}
+                    </li>
+                    <li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1" data-thumb="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" >
+                        <img class="rSlider__img" src="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" width="947" height="532" alt="#alt">
+                        {!! file_get_contents(public_path('svg/inline-play-button.svg')) !!}
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-three.png">
+                        <img class="rSlider__img" src="./img/rea-three.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-four.png">
+                        <img class="rSlider__img" src="./img/rea-four.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-five.png">
+                        <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-five.png">
+                        <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-five.png">
+                        <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb="./img/rea-five.png">
+                        <img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
+                    </li>
+                    <li class="rSlider__item" data-thumb=""></li>
+                </ul>
+            </div>
+
+            <aside class="rea-infos">
+                <h3 role="heading" aria-level="3" class="rea-infos__title">Détails sur le projet</h3>
+                <dl class="rea-infos__list">
+                    <dt class="rea-infos__term">Orentation</dt>
+                        <dd class="rea-infos__data">Web</dd>
+                    <dt class="rea-infos__term">Année de réalisation</dt>
+                        <dd class="rea-infos__data">2016</dd>
+                    <dt class="rea-infos__term">Type de projet</dt>
+                        <dd class="rea-infos__data">Site web</dd>
+                    <dt class="rea-infos__term">Cadre</dt>
+                        <dd class="rea-infos__data">Cours de design web</dd>
+                        <dd class="rea-infos__data">Projet pour un client</dd>
+                </dl>
+                <div class="rea-share">
+                    <span class="rea-share__label">Partager sur les réseaux sociaux</span>
+                    <ul class="social-list-circle">
+                        <li class="social-list-circle__item">
+                            <a href="" class="social-list-circle__link facebook" rel="me"><span>Partager le projet « Sur un Baobab » sur Facebook</span></a>
+                        </li><!--
+                --><li class="social-list-circle__item">
+                            <a href="" class="social-list-circle__link twitter" rel="me"><span>Partager le projet « Sur un Baobab » sur Twitter</span></a>
+                        </li><!--
+                --><li class="social-list-circle__item">
+                            <a href="" class="social-list-circle__link pinterest" rel="me"><span>Partager le projet « Sur un Baobab » sur Pinterest</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+
         </div>
+
 
 
     </section>
