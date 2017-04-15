3a671d8a54afee85f3b57158e229bf7a041650cb
Jimmy <jimmy@letecheur.me>
2017-04-15 18:42:14 +0200

    create header blog and little anim svg

--- ecoleinfographie/resources/views/partials/blog/header-blog.blade.php
+++ ecoleinfographie/resources/views/partials/blog/header-blog.blade.php
@@ -6,10 +6,12 @@
         <a href="#home" class="logo">Logo</a>
         @include('partials.navigation')
         @include('partials.breadcrumb')
-        <div class="header-rea">
-            <strong class="header-rea__title">Le blog de l’infographie</strong>
-            <p class="header-rea__intro">Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus, quis mollis sollicitudin, erat a elementum nsi lum terra...</p>
+        <div class="header-home">
+            <strong class="header-home__title">Le blog de l’infographie</strong>
+            <p class="header-home__intro">Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus, quis mollis sollicitudin, erat a elementum nsi lum terra...</p>
         </div>
     </div>
-    <div class="blog-img">{!! file_get_contents(public_path('svg/blog.svg')) !!}</div>
+    <div class="blog-home-img">
+        {!! file_get_contents(public_path('svg/blog.svg')) !!}
+    </div>
 </header>
