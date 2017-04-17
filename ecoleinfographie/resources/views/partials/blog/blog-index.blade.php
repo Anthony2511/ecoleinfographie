025412e88f637440e28ac40be90658a9ffb9b4bb
Jimmy <jimmy@letecheur.me>
2017-04-17 18:06:11 +0200

    create style for pagination links blog

--- ecoleinfographie/resources/views/partials/blog/blog-index.blade.php
+++ ecoleinfographie/resources/views/partials/blog/blog-index.blade.php
@@ -155,6 +155,56 @@
                 </footer>
 
             </article>
+            <article class="blog-card">
+                <a href="#goArticle" class="blog-card__go-article"><span>Lire l’article NOM ARTICLE</span></a>
+                <header class="blog-card__header">
+                    <a href="#category" class="blog-card__category blog-card__category--3D">3D/Vidéo</a>
+                    <img src="./img/blog-img-min3.jpg" width="358" height="264" alt="" class="blog-card__img">
+                </header>
+                <h3 role="heading" aria-level="3" class="blog-card__title">Hidrim asu elt artec indu moro stigma</h3>
+                <footer class="blog-card__footer">
+                    <a href="#author" class="blog-card__author" rel=author>
+                        <img src="./img/blog-author-30x30.jpg" width="30" height="30" alt="" class="blog-card__author__img">
+                        <span class="blog-card__author__name">
+                            Par
+                            <span class="blog-card__author__name--hidden">Voir le profile de </span>
+                            <span class="blog-card__author__name--container">
+                                <span class="blog-card__author__name--name">Nicolas Claisse</span>
+                            </span>
+                        </span>
+                    </a>
+                    <a href="#comments" class="blog-card__comments">
+                        <span class="blog-card__comments__number">5</span>
+                        <span class="blog-card__comments__hidden"> commentaires à voir sur l’article NOM ARTICLE</span>
+                    </a>
+                </footer>
+
+            </article>
+            <article class="blog-card">
+                <a href="#goArticle" class="blog-card__go-article"><span>Lire l’article NOM ARTICLE</span></a>
+                <header class="blog-card__header">
+                    <a href="#category" class="blog-card__category blog-card__category--2D">Design graphique</a>
+                    <img src="./img/blog-img-min2.jpg" width="358" height="264" alt="" class="blog-card__img">
+                </header>
+                <h3 role="heading" aria-level="3" class="blog-card__title">Lorem ipsum dolor sit amet</h3>
+                <footer class="blog-card__footer">
+                    <a href="#author" class="blog-card__author" rel=author>
+                        <img src="./img/blog-author-30x30.jpg" width="30" height="30" alt="" class="blog-card__author__img">
+                        <span class="blog-card__author__name">
+                            Par
+                            <span class="blog-card__author__name--hidden">Voir le profile de </span>
+                            <span class="blog-card__author__name--container">
+                                <span class="blog-card__author__name--name">Maëlle Vivegnis</span>
+                            </span>
+                        </span>
+                    </a>
+                    <a href="#comments" class="blog-card__comments">
+                        <span class="blog-card__comments__number">5</span>
+                        <span class="blog-card__comments__hidden"> commentaires à voir sur l’article NOM ARTICLE</span>
+                    </a>
+                </footer>
+
+            </article>
 
         </div>
 
@@ -269,5 +319,30 @@
         </section>
 
     </aside>
+
+    <div class="blog-pagination">
+        <a href="#precedent" class="blog-pagination__button blog-pagination__button--prev blog-pagination__button--noclick" title="Aller à la page précédente">
+            <span class="blog-pagination__button--hidden">Page </span><span>Précédent</span>
+        </a>
+
+        <a href="#suivant" class="blog-pagination__button blog-pagination__button--next" title="Aller à la page suivante">
+            <span class="blog-pagination__button--hidden">Page </span><span>Suivant</span>
+        </a>
+
+        <ul class="blog-pagination__list">
+            <li class="blog-pagination__item"><a href="#number" class="blog-pagination__link blog-pagination__link--active">1</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">2</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">3</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">4</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">5</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">6</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">7</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">8</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">9</a></li><!--
+        --><li class="blog-pagination__item"><a href="#number" class="blog-pagination__link">10</a></li>
+        </ul>
+
+
+    </div>
 
 </div>
