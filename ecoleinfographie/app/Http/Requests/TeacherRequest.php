3c9b3c854fc4f0a96b2b6bb23162c50a12988375
Jimmy <jimmy@letecheur.me>
2017-05-03 18:29:07 +0200

    Generate file TeacherRequest

---
+++ ecoleinfographie/app/Http/Requests/TeacherRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests;
+
+class ArticleRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return \Auth::check();
+    }
+}
