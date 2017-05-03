5eeea9e404de6167fa50a54567ae16f9e2375e5c
Jimmy <jimmy@letecheur.me>
2017-05-03 17:49:51 +0200

    create migration for course table

---
+++ ecoleinfographie/database/migrations/2017_05_03_153758_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCoursesTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('courses', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('slug', 255);
+			$table->string('name', 255);
+			$table->string('orientation', 255);
+			$table->integer('duration');
+			$table->integer('ects');
+			$table->text('ratio');
+			$table->text('evaluation');
+			$table->integer('bloc');
+			$table->string('quadrimester', 255);
+			$table->string('shortdescription', 255);
+			$table->text('description');
+			$table->text('objectives');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('courses');
+	}
+}
