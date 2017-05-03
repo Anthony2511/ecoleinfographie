485d8e43731ec38afdf41d226148ac6b0045cfe4
Jimmy <jimmy@letecheur.me>
2017-05-03 17:49:41 +0200

    create migration for teacher table

---
+++ ecoleinfographie/database/migrations/2017_05_03_153758_create_teachers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTeachersTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('teachers', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('slug', 255);
+			$table->string('lastname', 255);
+			$table->string('firstname', 255);
+			$table->string('role', 255);
+			$table->text('description');
+			$table->string('picture', 255)->nullable();
+			$table->string('email', 255)->nullable();
+			$table->text('social')->nullable();
+			$table->enum('status', array('VISIBLE', 'INVISIBLE'));
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('teachers');
+	}
+}
