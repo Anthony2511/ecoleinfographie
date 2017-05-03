90d5401c66bc7f8285d620e74577c7dbcbe36928
Jimmy <jimmy@letecheur.me>
2017-05-03 20:57:57 +0200

    fill model Course

--- ecoleinfographie/app/Models/Course.php
+++ ecoleinfographie/app/Models/Course.php
@@ -2,51 +2,90 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
     use CrudTrait;
-
-     /*
-    |--------------------------------------------------------------------------
-    | GLOBAL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-
-    //protected $table = 'courses';
-    //protected $primaryKey = 'id';
-    // public $timestamps = false;
+    use Sluggable;
+    
+    /*
+   |--------------------------------------------------------------------------
+   | GLOBAL VARIABLES
+   |--------------------------------------------------------------------------
+   */
+    
+    protected $table = 'courses';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
     // protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = [
+        'slug',
+        'name',
+        'orientation',
+        'duration',
+        'ects',
+        'ratio',
+        'evaluation',
+        'bloc',
+        'quadrimester',
+        'shortdescription',
+        'description',
+        'objectives'
+    ];
     // protected $hidden = [];
     // protected $dates = [];
-
+    
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | SCOPES
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | ACCESORS
     |--------------------------------------------------------------------------
     */
-
+    
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+        
+        return $this->name;
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
