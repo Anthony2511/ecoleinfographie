a7fb1c2c76e0c4949781ef4a450b6faea3057c92
Jimmy <jimmy@letecheur.me>
2017-05-03 22:20:09 +0200

    create menu link in admin page for courses

--- ecoleinfographie/resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ ecoleinfographie/resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -50,6 +50,10 @@
           <li>
             <a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/teacher') }}"><i class="fa fa-address-card"></i><span>Professeurs</span></a></li>
 
+          <!-- professeurs -->
+          <li>
+            <a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/cours') }}"><i class="fa fa-puzzle-piece"></i><span>Liste des cours</span></a></li>
+
 
           <!-- item MenuCRUD -->
           <li><a href="{{ url('admin/menu-item') }}"><i class="fa fa-list"></i> <span>Menu</span></a></li>
