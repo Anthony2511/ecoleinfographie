ce5de0520d1ca0a23af71a203c8ef7f4b3c9fb3f
Jimmy <jimmy@letecheur.me>
2017-05-04 11:38:53 +0200

    Create model CourseTeacher

---
+++ ecoleinfographie/app/Models/CourseTeacher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseTeacher extends Model
+{
+    
+    protected $table = 'course_teacher';
+    public $timestamps = true;
+    
+}
