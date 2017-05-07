c8c20a3b7283ab5801abce0ecd32090bb97242e0
Jimmy <jimmy@letecheur.me>
2017-05-07 14:16:04 +0200

    modify courseController

--- ecoleinfographie/app/Http/Controllers/CourseController.php
+++ ecoleinfographie/app/Http/Controllers/CourseController.php
@@ -4,26 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Course;
-use App\Models\Page;
 use SEO;
 
 class CourseController extends Controller
 {
     protected function indexWeb()
     {
-        $page = Page::findBySlug('programme-des-cours-web');
-    
-        $this->data['title'] = $page->title;
-        $this->data['page']  = $page->withFakes();
-    
-        return view('pages.coursesWeb', [
+        return view('pages.web.program', [
             'getAllCoursesBloc1' => $this->getAllCoursesBloc1(),
             'getAllCoursesBloc2' => $this->getAllCoursesBloc2(),
             'getAllCoursesBloc3' => $this->getAllCoursesBloc3(),
             'getWebCoursesBloc1' => $this->getWebCoursesBloc1(),
             'getWebCoursesBloc2' => $this->getWebCoursesBloc2(),
             'getWebCoursesBloc3' => $this->getWebCoursesBloc3(),
-        ], $this->data);
+        ]);
     }
     
     protected function show($slug)
