5ae8b6df556ce426349a9eb5d40aa08a9e328f4c
Jimmy <jimmy@letecheur.me>
2017-05-09 15:06:32 +0200

    create migration for students

---
+++ ecoleinfographie/database/migrations/2017_05_09_130443_create_students_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Students', function(Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('slug', 255);
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('image', 255)->nullable();
+            $table->string('profession', 255);
+            $table->integer('year');
+            $table->string('orientation', 1000);
+            $table->boolean('is_freelance');
+            $table->string('company', 255)->nullable();
+            $table->string('social', 5000)->nullable();
+            $table->boolean('has_interview');
+            $table->text('interview')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Students');
+    }
+}
