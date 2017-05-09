9756a2ada4fb4704d3b7c4eb47490969ca980a58
Jimmy <jimmy@letecheur.me>
2017-05-09 15:21:15 +0200

    create base model file for Students

--- ecoleinfographie/app/Models/Student.php
+++ ecoleinfographie/app/Models/Student.php
@@ -2,51 +2,113 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+//use Backpack\CRUD\ModelTraits\SpatieTranslatable\Sluggable;
+//use Backpack\CRUD\ModelTraits\SpatieTranslatable\SluggableScopeHelpers;
 
 class Student extends Model
 {
     use CrudTrait;
-
-     /*
-    |--------------------------------------------------------------------------
-    | GLOBAL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-
-    //protected $table = 'students';
-    //protected $primaryKey = 'id';
-    // public $timestamps = false;
+    use Sluggable;
+    use HasTranslations;
+    
+    /*
+   |--------------------------------------------------------------------------
+   | GLOBAL VARIABLES
+   |--------------------------------------------------------------------------
+   */
+    
+    protected $table = 'students';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
     // protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = [
+        'slug',
+        'firstname',
+        'lastname',
+        'image',
+        'profession',
+        'year',
+        'orientation',
+        'is_freelance',
+        'company',
+        'social',
+        'has_interview',
+        'interview'
+    ];
     // protected $hidden = [];
     // protected $dates = [];
-
+    protected $translatable = ['profession', 'interview'];
+    
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | SCOPES
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | ACCESORS
     |--------------------------------------------------------------------------
     */
-
+    
+    public function getFirstnameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+    
+    public function getLastnameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+    
+    public function getFullNameAttribute()
+    {
+        return ucfirst($this->firstname) . ' ' . ucfirst($this->lastname);
+    }
+    
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+        
+        $lastname  = $this->lastname;
+        $firstname = $this->firstname;
+        
+        return $firstname . '-' . $lastname;
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
