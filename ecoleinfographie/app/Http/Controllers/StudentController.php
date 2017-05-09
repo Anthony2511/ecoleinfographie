87ed7894757f7dcce359ece02952cdbfb426bbf9
Jimmy <jimmy@letecheur.me>
2017-05-09 21:14:32 +0200

    Fill StudentController

--- ecoleinfographie/app/Http/Controllers/StudentController.php
+++ ecoleinfographie/app/Http/Controllers/StudentController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+use SEO;
 
 class StudentController extends Controller
 {
-    public function indexWeb()
+    public function indexWeb(Student $student)
     {
-        return view('pages.web.parcours');
+        SEO::setTitle('Le parcours des anciens diplômés en web');
+        SEO::setDescription('Dans notre école, nous te formons à exercer un métier. Quoi de mieux que de te montrer une sélection de nos anciens bacheliers qui exercent aujourd’hui un métier dans le web.');
+    
+        
+        
+        return view('pages.web.parcours', [
+            'getStudentsWebWithInterview' => $this->getStudentsWebWithInterview(),
+            'imageStudentCard' => $student->getImageStudent('_cards.jpg')
+            
+        ]);
+    }
+    
+    public function getStudentsWebWithInterview()
+    {
+        $students = Student::where('has_interview', 1)->where('orientation', 'web')->get();
+        return $students;
     }
 }
