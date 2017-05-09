0c282d984a7f83c647705751e846e12018e622ba
Jimmy <jimmy@letecheur.me>
2017-05-09 15:13:24 +0200

    generate base file for CrudController Students

--- ecoleinfographie/app/Models/Student.php
+++ ecoleinfographie/app/Models/Student.php
@@ -1,64 +1,26 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\CrudTrait;
 
 class Student extends Model
 {
+    use CrudTrait;
+
      /*
     |--------------------------------------------------------------------------
     | GLOBAL VARIABLES
     |--------------------------------------------------------------------------
     */
 
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    // protected $table = 'interviews';
-
-    /**
-     * The primary key for the model.
-     *
-     * @var string
-     */
-    // protected $primaryKey = 'id';
-
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var boolean
-     */
+    //protected $table = 'students';
+    //protected $primaryKey = 'id';
     // public $timestamps = false;
-
-    /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
     // protected $guarded = ['id'];
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     // protected $fillable = [];
-
-    /**
-     * The attributes that should be hidden for arrays
-     *
-     * @var array
-     */
     // protected $hidden = [];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
     // protected $dates = [];
 
     /*
