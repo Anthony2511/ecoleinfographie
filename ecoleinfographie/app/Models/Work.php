7d256e9deee15c5e2d5c4b626ceaf1573d1b1da6
Jimmy <jimmy@letecheur.me>
2017-05-11 22:04:31 +0200

    fill model Work

--- ecoleinfographie/app/Models/Work.php
+++ ecoleinfographie/app/Models/Work.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+//use Backpack\CRUD\ModelTraits\SpatieTranslatable\Sluggable;
+//use Backpack\CRUD\ModelTraits\SpatieTranslatable
+use Backpack\CRUD\ModelTraits\SpatieTranslatable\HasTranslations;
 
 class Work extends Model
 {
     use CrudTrait;
+    use Sluggable;
+    use HasTranslations;
 
      /*
     |--------------------------------------------------------------------------
@@ -15,13 +21,36 @@ class Work extends Model
     |--------------------------------------------------------------------------
     */
 
-    //protected $table = 'works';
-    //protected $primaryKey = 'id';
-    // public $timestamps = false;
+    protected $table = 'works';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
     // protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = [
+        'slug',
+        'title',
+        'orientation',
+        'year',
+        'project_link',
+        'view3D',
+        'video',
+        'cover',
+        'images',
+        'description',
+        'other_description',
+        'other_link'
+    ];
     // protected $hidden = [];
     // protected $dates = [];
+    protected $translatable = ['title', 'description', 'other_description'];
+    
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
 
     /*
     |--------------------------------------------------------------------------
