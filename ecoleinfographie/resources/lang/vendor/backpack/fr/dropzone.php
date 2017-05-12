32ab3cd7dfd25958ca7f4934dd036bae357920cb
Jimmy <jimmy@letecheur.me>
2017-05-12 15:22:01 +0200

    publish defaults views and field backpack

---
+++ ecoleinfographie/resources/lang/vendor/backpack/fr/dropzone.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'drop_to_upload' => 'Déposer des fichiers ici pour les télécharger (ou cliquez pour les selectionner)',
+    'not_supported' => 'Votre navigateur ne prend pas en charge les téléchargements de fichiers par glisser/déposer.',
+    'invalid_file_type' => 'Type de fichier invalide.',
+    'file_too_big' => 'Fichier trop lourd. Vous avez uploadé un fichier de {{filesize}} tandis que {{maxFilesize}} est la taille maximum autorisée.',
+    'response_error' => 'Le serveur a répondu avec une erreur: {{statusCode}}.',
+    'max_files_exceeded' => 'Vous avez dépassé le nombre maximum de fichiers à mettre en ligne.',
+    'cancel_upload' => 'Annuler la mise en ligne',
+    'cancel_upload_confirmation' => 'Mise en ligne annulée',
+    'remove_file' => 'Supprimer le fichier',
+];
