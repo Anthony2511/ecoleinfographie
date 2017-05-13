36353991fd84517048af1ec864b18e1a77b9be2f
Jimmy <jimmy@letecheur.me>
2017-05-13 12:15:18 +0200

    add datas to view item-realisations

--- ecoleinfographie/resources/views/partials/realisations/item-realisations.blade.php
+++ ecoleinfographie/resources/views/partials/realisations/item-realisations.blade.php
@@ -1,20 +1,27 @@
+@foreach($works as $work)
 <li class="reas__item">
-	<a href="une-realisation" class="reas__link">
+	<a href="#" class="reas__link">
 		<figure class="reas__figure">
-			<img class="reas__img" src="./img/reas-test.png" width="940" height="938" alt="">
+			<img class="reas__img" src="{{ $work->cover }}" width="940" height="938" alt="">
 			<figcaption class="reas__figcaption">
 				<div class="reas__section">
 					<span class="reas__section__name">
-						<span class="reas__section__name-text">Web</span>
+						<span class="reas__section__name-text">{{ $orientations[$work->orientation] }}</span>
 					</span>
 				</div>
 				<div class="reas__desc">
 					<div class="reas__desc-wrapper">
-						<strong class="reas__desc__name">Sur un Baobab</strong>
-						<span class="reas__desc__author">Par Jimmy Letecheur</span>
+						<strong class="reas__desc__name">{{ $work->title }}</strong>
+						<span class="reas__desc__author">
+							Par
+							@foreach($work->students as $student)
+									{{ $student->fullname }}
+							@endforeach
+						</span>
 					</div>
 				</div>
 			</figcaption>
 		</figure>
 	</a>
 </li>
+@endforeach
