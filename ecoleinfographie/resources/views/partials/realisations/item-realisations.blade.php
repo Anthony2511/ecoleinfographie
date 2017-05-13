79bcdff3e1df5221fc63739afd793210b117d678
Jimmy <jimmy@letecheur.me>
2017-05-13 11:42:55 +0200

    move item index réalisation to partial

---
+++ ecoleinfographie/resources/views/partials/realisations/item-realisations.blade.php
@@ -0,0 +1,20 @@
+<li class="reas__item">
+	<a href="une-realisation" class="reas__link">
+		<figure class="reas__figure">
+			<img class="reas__img" src="./img/reas-test.png" width="940" height="938" alt="">
+			<figcaption class="reas__figcaption">
+				<div class="reas__section">
+					<span class="reas__section__name">
+						<span class="reas__section__name-text">Web</span>
+					</span>
+				</div>
+				<div class="reas__desc">
+					<div class="reas__desc-wrapper">
+						<strong class="reas__desc__name">Sur un Baobab</strong>
+						<span class="reas__desc__author">Par Jimmy Letecheur</span>
+					</div>
+				</div>
+			</figcaption>
+		</figure>
+	</a>
+</li>
