dfe8cfb17f8e029c178a96e6b3d36a84f9b5ce6e
Jimmy <jimmy@letecheur.me>
2017-05-15 00:19:33 +0200

    implements gallery slideshow in view

--- ecoleinfographie/resources/views/posts/postWork.blade.php
+++ ecoleinfographie/resources/views/posts/postWork.blade.php
@@ -23,33 +23,20 @@
 
 				<div class="rSlider__container">
 					<ul class="rSlider">
-						<li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1" data-thumb="./img/rea-3D.png">
+						{{--<li class="rSlider__item rSlider__item--withIframe" data-iframe="https://sketchfab.com/models/ba2674954ebb4c1eb4c2d8687b2013f0/embed?autostart=1" data-thumb="./img/rea-3D.png">
 							<img class="rSlider__img" src="./img/rea-3D.png" width="947" height="532" alt="#alt">
 							{!! file_get_contents(public_path('svg/inline-play-button3D.svg')) !!}
-						</li>
-						<li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1" data-thumb="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" >
+						</li>--}}
+						{{--<li class="rSlider__item rSlider__item--withIframe" data-iframe="//www.youtube.com/embed/c0asJgSyxcY?autoplay=1" data-thumb="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" >
 							<img class="rSlider__img" src="https://img.youtube.com/vi/shU6mXrlB4g/maxresdefault.jpg" width="947" height="532" alt="#alt">
 							{!! file_get_contents(public_path('svg/inline-play-button.svg')) !!}
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-three.png">
-							<img class="rSlider__img" src="{{ asset('img/rea-three.png') }}" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-four.png">
-							<img class="rSlider__img" src="./img/rea-four.png" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-five.png">
-							<img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-five.png">
-							<img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-five.png">
-							<img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb="./img/rea-five.png">
-							<img class="rSlider__img" src="./img/rea-five.png" width="947" height="532" alt="#alt">
-						</li>
-						<li class="rSlider__item" data-thumb=""></li>
+						</li>--}}
+						@foreach($work->images as $image)
+							<li class="rSlider__item rSlider__item--visible" data-thumb="{{ url('uploads/works/gallery/'.basename($image, '.jpg') . '_sliderMin.jpg') }}" data-src="{{ url('uploads/works/gallery/'.basename($image, '.jpg') . '_sliderMax.jpg') }}">
+								<img class="rSlider__img" src="{{ url('uploads/works/gallery/'.basename($image, '.jpg') . '_sliderMin.jpg') }}" width="947" height="532" alt="#alt">
+							</li>
+						@endforeach
+						<li class="rSlider__item" data-thumb="" data-src=""></li>
 					</ul>
 				</div>
 
