fffbe6733ff7982aaf506bbbe9255f456cee2e1b
Jimmy <jimmy@letecheur.me>
2017-05-14 15:14:10 +0200

    delete older template

--- ecoleinfographie/resources/views/pages/OneRealisation.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('header')
-    @include('partials.header-min')
-@endsection
-
-@section('content')
-    @include('partials.breadcrumb')
-    @include('partials.realisations.post-realisation')
-@endsection
