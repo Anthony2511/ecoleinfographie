f1616f4f3777e2cc2a6f9fbbfb5bd34e2903f05b
Jimmy <jimmy@letecheur.me>
2017-05-14 15:15:08 +0200

    modify JS and class CSS

--- ecoleinfographie/resources/assets/js/front.js
+++ ecoleinfographie/resources/assets/js/front.js
@@ -32,7 +32,7 @@ var w = {
             if (w.$body.hasClass('les-metiers-du-web')) return 'metiersDuWeb';
             if (w.$body.hasClass('programme-des-cours')) return 'programCourses';
             if (w.$body.hasClass('student-post')) return 'oneFormerStudent';
-            if (w.$body.hasClass('oneRea')) return 'oneRea';
+            if (w.$body.hasClass('work')) return 'oneRea';
             if (w.$body.hasClass('blog')) return 'blog';
             if (w.$body.hasClass('postBlog')) return 'postBlog';
             if (w.$body.hasClass('graduate')) return 'graduated';
