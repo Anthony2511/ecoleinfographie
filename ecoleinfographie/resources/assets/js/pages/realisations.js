08b406f8d63c91f08c8a890d5d2761ca6b6fdb36
Jimmy <jimmy@letecheur.me>
2017-05-14 12:18:24 +0200

    create ajax realisation.js

--- ecoleinfographie/resources/assets/js/pages/realisations.js
+++ ecoleinfographie/resources/assets/js/pages/realisations.js
@@ -2,6 +2,7 @@ w.realisations = {
     oConf: {},
 
     init: function () {
+        this.initAjax();
         this.initGrids();
     },
 
@@ -18,5 +19,35 @@ w.realisations = {
     initGrids: function () {
         var grid = document.querySelector('.reas');
         waterfall(grid);
-    }
+    },
+
+    initAjax: function () {
+
+        var button = $('#load-more');
+
+        $(document).ready(function() {
+            button.on('click', function(e){
+                e.preventDefault();
+                var page = $(this).attr('href');
+                if (typeof page !== 'undefined' && page.search('page=') !== -1 ){
+                    $.ajax({
+                        url: page,
+                        beforeSend: function () {
+                            button.addClass('loading');
+                        },
+                        success: function (data) {
+                            $('.reas').append(data.works);
+                            button.attr('href', data.next_page);
+                            button.removeClass('loading');
+                            w.realisations.initGrids();
+                        }
+                    })
+                } else {
+                    button.addClass('finish');
+                    button.replaceWith($('<span class="load-more finish">' + this.innerHTML + '</span>'));
+                    $('.load-more__label-text').text('Vous avez tout vu !');
+                }
+            })
+        })
+    },
 }
