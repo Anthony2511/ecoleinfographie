077cb63481f48ed46c5ac308279dc97c973332ae
Jimmy <jimmy@letecheur.me>
2017-05-16 11:55:48 +0200

    Create ArticleController

---
+++ ecoleinfographie/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use SEO;
+
+class ArticleController extends Controller
+{
+    protected function index(Article $article)
+    {
+        SEO::setTitle('Le blog de l’infographie');
+        SEO::setDescription('Découvres des articles sur l’infographie, sur le web, la réalisation 3D et vidéographique ainsi que sur le design graphique');
+        
+        return view('pages.blog');
+    }
+    
+    
+}
