bf2544ca7d80fbcf4a5cced8138be10f92b4dc9f
Jimmy <jimmy@letecheur.me>
2017-05-16 11:56:13 +0200

    Modify header blog

--- ecoleinfographie/resources/views/partials/blog/header-blog.blade.php
+++ ecoleinfographie/resources/views/partials/blog/header-blog.blade.php
@@ -1,11 +1,33 @@
 <header class="header header-shawl" role="banner">
     <div class="header__wrapper">
         <h1 role="heading" aria-level="1" class="header-min__title">
-			<?= $page->title ?> | École d’infographie de la Province de Liège
+			      Le blog | École d’infographie de la Province de Liège
         </h1>
         <a href="/" class="logo">Logo</a>
         @include('partials.navigation')
-        @include('partials.breadcrumb')
+
+        <div class="breadcrumb breadcrumb--header">
+            <ol class="breadcrumb__list" itemscope itemtype="http://schema.org/BreadcrumbList">
+                <li class="breadcrumb__item" itemprop="itemListElement" itemscope
+                    itemtype="http://schema.org/ListItem">
+                    <a href="#" class="breadcrumb__link breadcrumb__link--home" itemscope itemtype="http://schema.org/Thing"
+                       itemprop="item">
+                        <span itemprop="name">Page d’accueil</span>
+                    </a>
+                    <meta itemprop="position" content="1" />
+                </li>
+                <li class="breadcrumb__item" itemprop="itemListElement" itemscope
+                    itemtype="http://schema.org/ListItem">
+                    <a href="#" class="breadcrumb__link breadcrumb__link--active" itemscope itemtype="http://schema.org/Thing"
+                       itemprop="item">
+                        <span itemprop="name">Le blog de l’infographie</span>
+                    </a>
+                    <meta itemprop="position" content="2" />
+                </li>
+            </ol>
+        </div>
+
+
         <div class="header-home">
             <strong class="header-home__title">Le blog de l’infographie</strong>
             <p class="header-home__intro">Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus, quis mollis sollicitudin, erat a elementum nsi lum terra...</p>
