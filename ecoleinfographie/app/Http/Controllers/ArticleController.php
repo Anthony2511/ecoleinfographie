f790092679e7af452cf227efd9b98056d5c53197
Jimmy <jimmy@letecheur.me>
2017-05-16 22:06:24 +0200

    Create query for get articles & method orientation

--- ecoleinfographie/app/Http/Controllers/ArticleController.php
+++ ecoleinfographie/app/Http/Controllers/ArticleController.php
@@ -6,13 +6,35 @@ use App\Models\Article;
 use Illuminate\Http\Request;
 use SEO;
 
+
 class ArticleController extends Controller
 {
-    protected function index(Article $article)
+    protected function index()
     {
         SEO::setTitle('Le blog de l’infographie');
         SEO::setDescription('Découvres des articles sur l’infographie, sur le web, la réalisation 3D et vidéographique ainsi que sur le design graphique');
         
-        return view('pages.blog');
+        
+        $articles = Article::published()->paginate(8);
+        
+        return view('pages.blog', [
+            'articles' => $articles,
+            'orientation' => $this->getOrientation()
+        ]);
     }
+    
+    public function getOrientation()
+    {
+        $orientations = [
+            '2D'  => trans('programCourse.2D'),
+            '3D'  => trans('programCourse.3D'),
+            'web' => trans('programCourse.web')
+        ];
+        
+        return $orientations;
+    }
+    
+    
+    
+    
 }
