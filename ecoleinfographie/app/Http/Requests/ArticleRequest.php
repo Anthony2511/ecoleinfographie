03215eb56d2525d1e6957f4d63de4aaf2b466dc6
Jimmy <jimmy@letecheur.me>
2017-05-16 21:29:44 +0200

    add rules for orientation ArticleRequest

--- ecoleinfographie/app/Http/Requests/ArticleRequest.php
+++ ecoleinfographie/app/Http/Requests/ArticleRequest.php
@@ -27,6 +27,7 @@ class ArticleRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
         return [
             'title'        => 'required|min:2|max:255',
             'slug'         => 'unique:articles,slug,' . \Request::get('id'),
+            'orientation'  => 'required',
             'image'        => 'required',
             'introduction' => 'required|min:50',
             'content'      => 'required|min:255',
