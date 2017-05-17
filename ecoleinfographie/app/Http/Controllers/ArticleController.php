cb21f7dbcfe8ae1b05133a94b41e38dd455f56c5
Jimmy <jimmy@letecheur.me>
2017-05-17 22:24:20 +0200

    perform ARticleController

--- ecoleinfographie/app/Http/Controllers/ArticleController.php
+++ ecoleinfographie/app/Http/Controllers/ArticleController.php
@@ -12,30 +12,50 @@ class ArticleController extends Controller
     protected function index(Request $request)
     {
         $articles = Article::published()->paginate(8);
-    
-        if ($request->has('search'))
-        {
-            $search = $request->get('search');
-            $articles = Article::published()
-                ->where('title', 'LIKE', '%' . $search . '%')
-                ->orWhere('content', 'LIKE', '%' . $search . '%')
-                ->orWhere('introduction', 'LIKE', '%' . $search . '%')
-                ->paginate(8);
         
-            return view('pages.blog', [
-                'articles' => $articles,
-                'orientation' => $this->getOrientation()
-            ]);
+        
+        if ($request->has('search')) {
+            return $this->search($request);
+        }
+        
+        if ($request->has('category')) {
+            return $this->getCategory($request);
         }
         
         return view('pages.blog', [
             'articles'    => $articles,
-            'orientation' => $this->getOrientation(),
+            'orientation' => $this->getOrientation()
         ]);
     }
     
+    public function getCategory($request)
+    {
+        $search = $request->get('category');
+        
+        $articles = Article::published()
+                           ->where('orientation', 'LIKE', '%' . $search . '%')
+                           ->paginate(8);
+        
+        return view('pages.blog', [
+            'articles'    => $articles,
+            'orientation' => $this->getOrientation()
+        ]);
+    }
     
-
+    public function search($request)
+    {
+        $search   = $request->get('search');
+        $articles = Article::published()
+                           ->where('title', 'LIKE', '%' . $search . '%')
+                           ->orWhere('content', 'LIKE', '%' . $search . '%')
+                           ->orWhere('introduction', 'LIKE', '%' . $search . '%')
+                           ->paginate(8);
+        
+        return view('pages.blog', [
+            'articles'    => $articles,
+            'orientation' => $this->getOrientation()
+        ]);
+    }
     
     public function getOrientation()
     {
