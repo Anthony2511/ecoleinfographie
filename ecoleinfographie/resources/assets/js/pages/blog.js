3bfca4c2ed8090bc1c0104839900dc29d5acd62e
Jimmy <jimmy@letecheur.me>
2017-05-17 20:12:55 +0200

    create js for autocomplete

--- ecoleinfographie/resources/assets/js/pages/blog.js
+++ ecoleinfographie/resources/assets/js/pages/blog.js
@@ -3,6 +3,7 @@ w.blog = {
 
     init: function () {
         w.asideBlog.init();
+        this.autocomplete();
     },
 
     // getElements: function () {
@@ -15,4 +16,33 @@ w.blog = {
     // EVENTS
 
     // FUNCTIONS
+    autocomplete: function () {
+        console.log('jqueryUiAutomplete Charged');
+        $(function () {
+            $("#search-blog").autocomplete({
+                source: "blog/search/autocomplete",
+                minLength: 3,
+                select: function (event, ui) {
+                    $('#search-blog').val(ui.item.value);
+                }
+            });
+        });
+    }
+
+    /*autocomplete: function () {
+     console.log('autocomplete charged');
+     var options = {
+     url: 'search/autocomplete',
+     getValue: 'title',
+     list: {
+     match: {
+     enabled: true
+     }
+     },
+
+     }
+
+     $( "#q" ).easyAutocomplete(options);
+     }*/
+
 }
