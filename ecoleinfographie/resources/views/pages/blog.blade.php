794814159f5888e759a85d55304627872700abfd
Jimmy <jimmy@letecheur.me>
2017-05-17 20:12:17 +0200

    import styles for jquery ui only on blog

--- ecoleinfographie/resources/views/pages/blog.blade.php
+++ ecoleinfographie/resources/views/pages/blog.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('class', 'blog-home blog')
+@section('styles')
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+@endsection
 
 @section('content')
     @include('partials.blog.blog-index')
--- ecoleinfographie/resources/views/partials/head.blade.php
+++ ecoleinfographie/resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
     {!! SEO::generate() !!}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @yield('styles')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
