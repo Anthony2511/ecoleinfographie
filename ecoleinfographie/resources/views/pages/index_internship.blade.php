009ddcd45b9739147ef0a73d4f4d2ee6d2048287
Jimmy <jimmy@letecheur.me>
2017-05-21 19:48:23 +0200

    html section propose

--- ecoleinfographie/resources/views/pages/index_internship.blade.php
+++ ecoleinfographie/resources/views/pages/index_internship.blade.php
@@ -28,5 +28,18 @@
 		</ol>
 	</div>
 
+	<section class="propose">
+		<h2 role="heading" aria-level="2" class="propose__title">Proposer une offre de stage</h2>
+		<p class="propose__paragraph">Maecenas tincidunt iaculis consectetur. Quisque quis suscipit orci, et tincidunt nunc. Mauris arcu lorem, aliquam vitae aliquet ac, egestas ut nisl. Sed bibendum ac justo non consectetur. Duis eleifend dolor enim, sed rutrum est dictum ut. Nulla neque risus, mattis pretium vulputate eu, ultrices ac elit. Vivamus in nunc nec ipsum luctus vestibulum. Pellentesque maximus lorem lobortis, vestibulum ante ac, efficitur augue. Quisque varius leo vitae aliquam volutpat.
+		</p>
+		<div class="propose__infos">
+			<span class="propose__info">Durée de <strong>12</strong> semaines</span>
+			<span class="propose__info">Du <strong>13</strong> janvier au <strong>30</strong> mars</span>
+			<span class="propose__info"><strong>Signature</strong> d’une convention</span>
+		</div>
+	</section>
+
+
+
 
 @endsection
