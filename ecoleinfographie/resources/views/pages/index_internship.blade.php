2de4fab21b436e4f44f33d888e95006ec9337699
Jimmy <jimmy@letecheur.me>
2017-05-21 22:08:51 +0200

    create style for introduction and buttons internship

--- ecoleinfographie/resources/views/pages/index_internship.blade.php
+++ ecoleinfographie/resources/views/pages/index_internship.blade.php
@@ -33,9 +33,24 @@
 		<p class="propose__paragraph">Maecenas tincidunt iaculis consectetur. Quisque quis suscipit orci, et tincidunt nunc. Mauris arcu lorem, aliquam vitae aliquet ac, egestas ut nisl. Sed bibendum ac justo non consectetur. Duis eleifend dolor enim, sed rutrum est dictum ut. Nulla neque risus, mattis pretium vulputate eu, ultrices ac elit. Vivamus in nunc nec ipsum luctus vestibulum. Pellentesque maximus lorem lobortis, vestibulum ante ac, efficitur augue. Quisque varius leo vitae aliquam volutpat.
 		</p>
 		<div class="propose__infos">
-			<span class="propose__info">Durée de <strong>12</strong> semaines</span>
-			<span class="propose__info">Du <strong>13</strong> janvier au <strong>30</strong> mars</span>
-			<span class="propose__info"><strong>Signature</strong> d’une convention</span>
+			<span class="propose__info propose__info--duration">Durée de <strong>12</strong> semaines</span>
+			<span class="propose__info propose__info--date">Du <strong>13</strong> janvier au <strong>30</strong> mars</span>
+			<span class="propose__info propose__info--convention"><strong>Signature</strong><br/> d’une convention</span>
+		</div>
+	</section>
+
+	<section class="form">
+		<div class="form__container">
+			<h2 role="heading" aria-level="2" class="form__title">Déposer une offre</h2>
+			<p class="form__paragraph">Vous reçevrez un email de confirmation de la part du professeur responsable une fois que votre offre aura été validée. Elle sera ensuite transmise aux élèves.</p>
+			<div class="form__button-container">
+				<button class="form__button form__button--detail form__button--active">
+					<span class="form__button__label">Je remplis un formulaire détaillé</span>
+				</button>
+				<button class="form__button form__button--pdf">
+					<span class="form__button__label">J’envoie une offre au format PDF</span>
+				</button>
+			</div>
 		</div>
 	</section>
 
