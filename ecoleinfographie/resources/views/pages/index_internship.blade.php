a5aeaa431b953065a98c80b0a8b725b0326a90e3
Jimmy <jimmy@letecheur.me>
2017-05-22 01:35:46 +0200

    styles for page internship finish

--- ecoleinfographie/resources/views/pages/index_internship.blade.php
+++ ecoleinfographie/resources/views/pages/index_internship.blade.php
@@ -53,131 +53,143 @@
 			</div>
 		</div>
 
-		{{ Form::open([ 'method' => 'POST', 'class' => 'form-full']) }}
-		<div class="form__container">
-			<div class="form__block">
-				<div class="form__wrapper form__wrapper--col2 form__wrapper--left">
-					<label for="name" class="form__label">Votre nom</label>
-					<input type="text" name="name" id="name" class="form__input floatLabel" value="">
-				</div>
-				<div class="form__wrapper form__wrapper--col2 form__wrapper--right">
-					<label for="company" class="form__label">Nom de l’entreprise concernée</label>
-					<input type="text" name="company" id="company" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<label for="subject" class="form__label">Quel est le sujet de votre demande ?</label>
-					<input type="text" name="subject" id="subject" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<label for="email" class="form__label">Quel est votre adresse e-mail ?</label>
-					<input type="email" name="email" id="email" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<span for="recipient" class="form__fakeLabel">À quelle option s’adresse votre offre&nbsp;?</span>
-					<div class="form__checkbox-btn">
-						<label for="cbox1" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">3D et Audiovisuel</span>
-							<input type="checkbox" id="cbox1" value="" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+		<div class="tab-wrapper">
+			<div class="tab tab--1 active">
+				{{ Form::open([ 'method' => 'POST', 'class' => 'form-full']) }}
+				<div class="form__container">
+					<div class="form__block">
+						<div class="form__wrapper form__wrapper--col2 form__wrapper--left">
+							<label for="name" class="form__label">Votre nom</label>
+							<input type="text" name="name" id="name" class="form__input floatLabel" value="">
+						</div>
+						<div class="form__wrapper form__wrapper--col2 form__wrapper--right">
+							<label for="company" class="form__label">Nom de l’entreprise concernée</label>
+							<input type="text" name="company" id="company" class="form__input floatLabel" value="">
+						</div>
 					</div>
-					<div class="form__checkbox-btn">
-						<label for="cbox2" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">Design graphique</span>
-							<input type="checkbox" id="cbox2" value="" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<label for="subject" class="form__label">Quel est le sujet de votre demande ?</label>
+							<input type="text" name="subject" id="subject" class="form__input floatLabel" value="">
+						</div>
 					</div>
-					<div class="form__checkbox-btn">
-						<label for="cbox3" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">Web</span>
-							<input type="checkbox" id="cbox3" value="" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<label for="email" class="form__label">Quel est votre adresse e-mail ?</label>
+							<input type="email" name="email" id="email" class="form__input floatLabel" value="">
+						</div>
 					</div>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<span for="recipient" class="form__fakeLabel">À quelle option s’adresse votre offre&nbsp;?</span>
+							<div class="form__checkbox-btn">
+								<label for="cbox1" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">3D et Audiovisuel</span>
+									<input type="checkbox" id="cbox1" value="" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+							<div class="form__checkbox-btn">
+								<label for="cbox2" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">Design graphique</span>
+									<input type="checkbox" id="cbox2" value="" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+							<div class="form__checkbox-btn">
+								<label for="cbox3" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">Web</span>
+									<input type="checkbox" id="cbox3" value="" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="form__block">
+						<div class="form__wrapperTextarea">
+							<label for="description" class="form__labelTextarea">Décrivez votre entreprise en quelques mots ?</label>
+							<textarea name="description" id="description" class="form__textarea"></textarea>
+						</div>
+					</div>
+					<div class="form__block">
+						<div class="form__wrapperTextarea">
+							<label for="profils" class="form__labelTextarea">Quels sont les profils que vous recherchez&nbsp;?</label>
+							<textarea name="profils" id="profils" class="form__textarea"></textarea>
+						</div>
+					</div>
+					<div class="form__block">
+						<div class="form__wrapperTextarea">
+							<label for="proposition" class="form__labelTextarea">Que proposez-vous&nbsp;?</label>
+							<textarea name="proposition" id="proposition" class="form__textarea"></textarea>
+						</div>
+					</div>
+					<button class="form__submit">Envoyer l’offre de stage</button>
 				</div>
+				{{ Form::close() }}
 			</div>
-			<div class="form__block">
-				<div class="form__wrapperTextarea">
-					<label for="description" class="form__labelTextarea">Décrivez votre entreprise en quelques mots ?</label>
-					<textarea name="description" id="description" class="form__textarea"></textarea>
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapperTextarea">
-					<label for="profils" class="form__labelTextarea">Quels sont les profils que vous recherchez&nbsp;?</label>
-					<textarea name="profils" id="profils" class="form__textarea"></textarea>
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapperTextarea">
-					<label for="proposition" class="form__labelTextarea">Que proposez-vous&nbsp;?</label>
-					<textarea name="proposition" id="proposition" class="form__textarea"></textarea>
-				</div>
-			</div>
-			<button class="form__submit">Envoyer l’offre de stage</button>
-		</div>
-		{{ Form::close() }}
 
-		{{ Form::open([ 'method' => 'POST', 'class' => 'form-pdf']) }}
-		<div class="form__container">
-			<div class="form__block">
-				<div class="form__wrapper form__wrapper--col2 form__wrapper--left">
-					<label for="name-pdf" class="form__label">Votre nom</label>
-					<input type="text" name="name-pdf" id="name-pdf" class="form__input floatLabel" value="">
-				</div>
-				<div class="form__wrapper form__wrapper--col2 form__wrapper--right">
-					<label for="company-pdf" class="form__label">Nom de l’entreprise concernée</label>
-					<input type="text" name="company-pdf" id="company-pdf" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<label for="subject-pdf" class="form__label">Quel est le sujet de votre demande ?</label>
-					<input type="text" name="subject-pdf" id="subject-pdf" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<label for="email-pdf" class="form__label">Quel est votre adresse e-mail ?</label>
-					<input type="email" name="email-pdf" id="email-pdf" class="form__input floatLabel" value="">
-				</div>
-			</div>
-			<div class="form__block">
-				<div class="form__wrapper">
-					<span for="recipient" class="form__fakeLabel">À quelle option s’adresse votre offre&nbsp;?</span>
-					<div class="form__checkbox-btn">
-						<label for="cbox1-pdf" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">3D et Audiovisuel</span>
-							<input type="checkbox" name="cbox1" id="cbox1-pdf" value="" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+			<div class="tab tab--2">
+				{{ Form::open([ 'method' => 'POST', 'class' => 'form-pdf']) }}
+				<div class="form__container">
+					<div class="form__block">
+						<div class="form__wrapper form__wrapper--col2 form__wrapper--left">
+							<label for="name-pdf" class="form__label">Votre nom</label>
+							<input type="text" name="name-pdf" id="name-pdf" class="form__input floatLabel" value="">
+						</div>
+						<div class="form__wrapper form__wrapper--col2 form__wrapper--right">
+							<label for="company-pdf" class="form__label">Nom de l’entreprise concernée</label>
+							<input type="text" name="company-pdf" id="company-pdf" class="form__input floatLabel" value="">
+						</div>
 					</div>
-					<div class="form__checkbox-btn">
-						<label for="cbox2-pdf" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">Design graphique</span>
-							<input type="checkbox" name="cbox2" id="cbox2-pdf" value="" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<label for="subject-pdf" class="form__label">Quel est le sujet de votre demande ?</label>
+							<input type="text" name="subject-pdf" id="subject-pdf" class="form__input floatLabel" value="">
+						</div>
 					</div>
-					<div class="form__checkbox-btn">
-						<label for="cbox3-pdf" class="form__labelCheckbox">
-							<span class="form__labelCheckbox__span">Web</span>
-							<input type="checkbox" id="cbox3-pdf" value="" name="cbox3" class="form__checkbox">
-							<div class="form__control-indicator"></div>
-						</label>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<label for="email-pdf" class="form__label">Quel est votre adresse e-mail ?</label>
+							<input type="email" name="email-pdf" id="email-pdf" class="form__input floatLabel" value="">
+						</div>
 					</div>
+					<div class="form__block">
+						<div class="form__wrapper form-wrapper--file">
+							<input type="file" name="file-pdf" id="file-pdf" class="form__inputFile" value="">
+							<label for="file-pdf" class="form__labelFile"><span>Choisissez un fichier</span></label>
+						</div>
+					</div>
+					<div class="form__block">
+						<div class="form__wrapper">
+							<span for="recipient" class="form__fakeLabel">À quelle option s’adresse votre offre&nbsp;?</span>
+							<div class="form__checkbox-btn">
+								<label for="cbox1-pdf" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">3D et Audiovisuel</span>
+									<input type="checkbox" name="cbox1" id="cbox1-pdf" value="" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+							<div class="form__checkbox-btn">
+								<label for="cbox2-pdf" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">Design graphique</span>
+									<input type="checkbox" name="cbox2" id="cbox2-pdf" value="" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+							<div class="form__checkbox-btn">
+								<label for="cbox3-pdf" class="form__labelCheckbox">
+									<span class="form__labelCheckbox__span">Web</span>
+									<input type="checkbox" id="cbox3-pdf" value="" name="cbox3" class="form__checkbox">
+									<div class="form__control-indicator"></div>
+								</label>
+							</div>
+						</div>
+					</div>
+					<button class="form__submit">Envoyer l’offre de stage</button>
 				</div>
+				{{ Form::close() }}
 			</div>
-			<button class="form__submit">Envoyer l’offre de stage</button>
 		</div>
-		{{ Form::close() }}
 
 	</section>
 
