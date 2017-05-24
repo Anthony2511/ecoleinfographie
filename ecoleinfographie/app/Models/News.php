c379dbb4e4d993a3cd0d86cb4714525532b659a6
Jimmy <jimmy@letecheur.me>
2017-05-24 12:03:32 +0200

    filling model News

--- ecoleinfographie/app/Models/News.php
+++ ecoleinfographie/app/Models/News.php
@@ -2,51 +2,99 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Backpack\CRUD\ModelTraits\SpatieTranslatable\HasTranslations;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
     use CrudTrait;
-
-     /*
-    |--------------------------------------------------------------------------
-    | GLOBAL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-
-    //protected $table = 'newss';
-    //protected $primaryKey = 'id';
-    // public $timestamps = false;
+    use Sluggable;
+    use HasTranslations;
+    
+    /*
+   |--------------------------------------------------------------------------
+   | GLOBAL VARIABLES
+   |--------------------------------------------------------------------------
+   */
+    
+    protected $table = 'news';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
     // protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = ['slug', 'title', 'date', 'content', 'image', 'status', 'featured'];
     // protected $hidden = [];
     // protected $dates = [];
-
+    protected $casts = [
+        'featured' => 'boolean',
+        'date' => 'date'
+    ];
+    protected $translatable = ['title', 'content'];
+    
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ]
+        ];
+    }
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
-
+    public function getDateForHuman()
+    {
+        if (App::getLocale() == 'fr') {
+            setlocale(LC_TIME, 'fr_FR');
+            
+            return Carbon::parse($this->date)->formatLocalized('%d %B %Y');
+        } else {
+            return Carbon::parse($this->date)->formatLocalized('%d %B %Y');
+        }
+    }
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
     |--------------------------------------------------------------------------
     */
-
+    
     /*
     |--------------------------------------------------------------------------
     | SCOPES
     |--------------------------------------------------------------------------
     */
-
+    
+    public function scopePublished($query)
+    {
+        return $query->where('status', 'PUBLIÉ')
+                     ->where('date', '<=', date('Y-m-d'))
+                     ->orderBy('date', 'DESC');
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | ACCESORS
     |--------------------------------------------------------------------------
     */
-
+    
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+        
+        return $this->title;
+    }
+    
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
