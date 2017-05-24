8fa42e3eddd8d4189a48cabd0a5ad2a1339b6621
Jimmy <jimmy@letecheur.me>
2017-05-24 13:50:11 +0200

    outsource somes parts in partials

---
+++ ecoleinfographie/resources/views/partials/news/cards.blade.php
@@ -0,0 +1,16 @@
+<article class="news-card">
+	<div class="news-card__content">
+		<h2 role="heading" aria-level="2" class="news-card__title">Les élèves de 3D et audiovisuel au FMX</h2>
+		<p class="news-card__excerpt">
+			Aliquam ornare rutrum lacus, bibendum viverra leo. Nunc felis tortor, facilisis vitae ante at, blandit euismod risus. Nunc tempor tempus volutpat. Phasellus venenatis lorem...
+		</p>
+		<span class="news-card__fakelink">Lire l’article</span>
+	</div>
+	<div class="news-card__image">
+		<figure class="news-card__figure">
+			<img src="{{ asset('img/news1.jpg') }}" width="503" height="447" alt="" class="news-card__img">
+		</figure>
+		<time class="news-card__time" datetime="2017-01-17" pubdate>10 janvier 2017</time>
+	</div>
+	<a href="#monarticle" class="news-card__link"><span>Lire l’article NOM ARTICLE</span></a>
+</article>
---
+++ ecoleinfographie/resources/views/partials/news/featured.blade.php
@@ -0,0 +1,18 @@
+<article class="featured">
+	<figure class="featured__figure">
+		<img class="featured__img" src="{{ asset('img/featured.jpg') }}" width="378" height="447" alt="">
+	</figure>
+	<div class="featured__content">
+		<span class="featured__date">Le
+			<time datetime="2017-01-13" pubdate>13 janvier 2017</time>
+		</span>
+		<h2 role="heading" aria-level="2" class="featured__title">Inscrivez-vous à la journée d’immersion</h2>
+		<p class="featured__excerpt">
+			Aliquam ornare rutrum lacus, bibendum viverra leo. Nunc felis tortor, facilisis vitae ante at, blandit euismod
+			risus. Nunc tempor tempus volutpat. Phasellus venenatis lorem vel faucibus ultrices. Aliquam in auctor sapien,
+			et lacinia est. Vivamus orci sem...
+		</p>
+		<span class="featured__fakelink">Lire l’article</span>
+	</div>
+	<a href="#monarticle" class="featured__link"><span>Lire l’article NOM ARTICLE</span></a>
+</article>
