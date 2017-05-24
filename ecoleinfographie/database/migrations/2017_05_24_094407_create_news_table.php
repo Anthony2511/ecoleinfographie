835d5c1018bcc37c80e8331eae32a6a9035b7b31
Jimmy <jimmy@letecheur.me>
2017-05-24 11:50:24 +0200

    create migration for news

---
+++ ecoleinfographie/database/migrations/2017_05_24_094407_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('title', 255);
+            $table->date('date');
+            $table->text('content');
+            $table->string('image', 255);
+            $table->enum('status', array('BROUILLON', 'PUBLIÉ'))->default('BROUILLON');
+            $table->boolean('featured')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('news');
+    }
+}
