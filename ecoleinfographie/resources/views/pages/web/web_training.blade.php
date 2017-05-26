2d5ac47605a34edf3bff0c1ad92ffc0a129498d3
Jimmy <jimmy@letecheur.me>
2017-05-26 16:35:09 +0200

    modify breadcrumb

--- ecoleinfographie/resources/views/pages/web/web_training.blade.php
+++ ecoleinfographie/resources/views/pages/web/web_training.blade.php
@@ -7,8 +7,36 @@
 @section('class', 'les-metiers-du-web-formation')
 
 @section('content')
-    @include('partials.breadcrumb')
-    @include('partials.web.intro-formation')
+	<div class="breadcrumb">
+		<ol class="breadcrumb__list" itemscope itemtype="http://schema.org/BreadcrumbList">
+			<li class="breadcrumb__item" itemprop="itemListElement" itemscope
+					itemtype="http://schema.org/ListItem">
+				<a href="{{ Url('/') }}" class="breadcrumb__link breadcrumb__link--home" itemscope itemtype="http://schema.org/Thing"
+					 itemprop="item">
+					<span itemprop="name">Page d’accueil</span>
+				</a>
+				<meta itemprop="position" content="1"/>
+			</li>
+			<li class="breadcrumb__item" itemprop="itemListElement" itemscope
+					itemtype="http://schema.org/ListItem">
+				<a href="{{ Route('webTrades') }}" class="breadcrumb__link breadcrumb__link--active" itemscope itemtype="http://schema.org/Thing"
+					 itemprop="item">
+					<span itemprop="name">Les métiers du web</span>
+				</a>
+				<meta itemprop="position" content="2"/>
+			</li>
+			<li class="breadcrumb__item" itemprop="itemListElement" itemscope
+					itemtype="http://schema.org/ListItem">
+				<a href="{{ Url()->current() }}" class="breadcrumb__link breadcrumb__link--active" itemscope itemtype="http://schema.org/Thing"
+					 itemprop="item">
+					<span itemprop="name">La formation</span>
+				</a>
+				<meta itemprop="position" content="2"/>
+			</li>
+		</ol>
+	</div>
+
+	@include('partials.web.intro-formation')
     @include('partials.web.blocs-formation')
 @endsection
 
