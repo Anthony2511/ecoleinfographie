2448e31ba73f0b0464bbaafec42712cb6ada3165
Jimmy <jimmy@letecheur.me>
2017-05-26 22:54:58 +0200

    responsive index news

--- ecoleinfographie/resources/views/partials/header-news.blade.php
+++ ecoleinfographie/resources/views/partials/header-news.blade.php
@@ -10,7 +10,7 @@
 			<ol class="breadcrumb__list" itemscope itemtype="http://schema.org/BreadcrumbList">
 				<li class="breadcrumb__item" itemprop="itemListElement" itemscope
 						itemtype="http://schema.org/ListItem">
-					<a href="#" class="breadcrumb__link breadcrumb__link--home" itemscope itemtype="http://schema.org/Thing"
+					<a href="{{ Url('/') }}" class="breadcrumb__link breadcrumb__link--home" itemscope itemtype="http://schema.org/Thing"
 						 itemprop="item">
 						<span itemprop="name">Page d’accueil</span>
 					</a>
@@ -18,9 +18,9 @@
 				</li>
 				<li class="breadcrumb__item" itemprop="itemListElement" itemscope
 						itemtype="http://schema.org/ListItem">
-					<a href="#" class="breadcrumb__link breadcrumb__link--active" itemscope itemtype="http://schema.org/Thing"
+					<a href="{{ Url()->current() }}" class="breadcrumb__link breadcrumb__link--active" itemscope itemtype="http://schema.org/Thing"
 						 itemprop="item">
-						<span itemprop="name">Actualité</span>
+						<span itemprop="name">Actualités</span>
 					</a>
 					<meta itemprop="position" content="2"/>
 				</li>
