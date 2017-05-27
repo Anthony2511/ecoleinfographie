68927deeb6c7c21e5d34da375c899ec22ca3ecc5
Jimmy <jimmy@letecheur.me>
2017-05-27 13:01:29 +0200

    update js resize

--- ecoleinfographie/resources/assets/js/pages/oneRea.js
+++ ecoleinfographie/resources/assets/js/pages/oneRea.js
@@ -1,26 +1,43 @@
 w.oneRea = {
-      oConf: {},
+    oConf: {},
 
-      init: function () {
+    init: function () {
         this.removeHidden();
         w.sliderRea.init();
-      },
+    },
 
-      // getElements: function () {
+    // getElements: function () {
 
-      // },
+    // },
 
-      // setEvents: function () {
+    // setEvents: function () {
 
-      // },
+    // },
 
-      // EVENTS
+    // EVENTS
 
-      // FUNCTIONS
+    // FUNCTIONS
 
-        removeHidden: function () {
-            if($(window).width() <= 1024) {
-                $('.rSlider__item--hidden').remove();
-            }
-        }
+    removeHidden: function () {
+
+        var resizeTimer;
+        var rmvEl = $('.rSlider__item--hidden');
+
+        $(window).on('resize', function (e) {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(function () {
+                if ($(window).width() <= 1024) {
+                    rmvEl.detach();
+                } else {
+                    rmvEl.append();
+                }
+            }, 250);
+        });
+
+        if($(window).width() <= 1024){
+            $('.rSlider__item--hidden').detach();
+        };
+
+
+    }
 }
