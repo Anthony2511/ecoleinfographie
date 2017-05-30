3ec941f82ee0c782ee658b54e6d6d981d62b2917
Jimmy <jimmy@letecheur.me>
2017-05-30 07:47:04 +0200

    check html blog list

--- ecoleinfographie/resources/views/partials/blog/aside.blade.php
+++ ecoleinfographie/resources/views/partials/blog/aside.blade.php
@@ -8,7 +8,6 @@
                 <label for="search-blog" class="blog-search__label">Je recherche...</label>
                 <input type="search" class="blog-search__input" name="search" id="search-blog">
                 <button class="blog-search__submit"><span>Lancer la recherche</span></button>
-            </form>
             {{ Form::close() }}
         </div>
     </section>
@@ -18,30 +17,30 @@
         <h3 role="heading" aria-level="3" class="blog-category__title">Catégories</h3>
         <ul class="blog-category__list">
             <li class="blog-category__main-item blog-category__main-item--web"><span>Web</span></li>
-            <ul class="blog-category__sublist">
+            <li class="blog-category__sublist">
                 @foreach($subCategoriesWeb as $category)
-                    <a href="{{ Route('blog') . '?category=web&subcategory=' . $category->slug  . '#anchor'}}"><li class="blog-category__subitem">{{ $category->name }}</li></a>
+                    <a href="{{ Route('blog') . '?category=web&subcategory=' . $category->slug  . '#anchor'}}"><span class="blog-category__subitem">{{ $category->name }}</span></a>
                 @endforeach
-                <a href="{{ Route('blog') . '?category=web#anchor' }}"><li class="blog-category__subitem">Tous les articles</li></a>
-            </ul>
+                <a href="{{ Route('blog') . '?category=web#anchor' }}"><span class="blog-category__subitem">Tous les articles</span></a>
+            </li>
         </ul>
         <ul class="blog-category__list">
             <li class="blog-category__main-item blog-category__main-item--2D"><span>Design graphique</span></li>
-            <ul class="blog-category__sublist">
+            <li class="blog-category__sublist">
                 @foreach($subCategories2d as $category)
-                    <a href="{{ Route('blog') . '?category=2D&subcategory=' . $category->slug . '#anchor'}}"><li class="blog-category__subitem">{{ $category->name }}</li></a>
+                    <a href="{{ Route('blog') . '?category=2D&subcategory=' . $category->slug . '#anchor'}}"><span class="blog-category__subitem">{{ $category->name }}</span></a>
                 @endforeach
-                <a href="{{ Route('blog') . '?category=2D#anchor' }}"><li class="blog-category__subitem">Tous les articles</li></a>
-            </ul>
+                <a href="{{ Route('blog') . '?category=2D#anchor' }}"><span class="blog-category__subitem">Tous les articles</span></a>
+            </li>
         </ul>
         <ul class="blog-category__list">
             <li class="blog-category__main-item blog-category__main-item--3D"><span>3D/Vidéo</span></li>
-            <ul class="blog-category__sublist">
+            <li class="blog-category__sublist">
                 @foreach($subCategories3d as $category)
-                    <a href="{{ Route('blog') . '?category=3D&subcategory=' . $category->slug . '#anchor'}}"><li class="blog-category__subitem">{{ $category->name }}</li></a>
+                    <a href="{{ Route('blog') . '?category=3D&subcategory=' . $category->slug . '#anchor'}}"><span class="blog-category__subitem">{{ $category->name }}</span></a>
                 @endforeach
-                <a href="{{ Route('blog') . '?category=3D#anchor' }}"><li class="blog-category__subitem">Tous les articles</li></a>
-            </ul>
+                <a href="{{ Route('blog') . '?category=3D#anchor' }}"><span class="blog-category__subitem">Tous les articles</span></a>
+            </li>
         </ul>
     </section>
 
