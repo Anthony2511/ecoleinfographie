b17e2b956a90ace368bf9f5cbf7d4b4056babf76
Jimmy <jimmy@letecheur.me>
2017-05-30 06:37:46 +0200

    add links and infos to footer

--- ecoleinfographie/resources/views/partials/footer.blade.php
+++ ecoleinfographie/resources/views/partials/footer.blade.php
@@ -6,31 +6,31 @@
         <link itemprop="url" href="http://ecoleinfographie.be">
         <ul class="social">
             <li class="social__item">
-                <a href="" itemprop="sameAs" rel="external" title="Vers Facebook" class="social__link social__link--facebook">
+                <a href="{{ Config::get('settings.social_fb') }}" itemprop="sameAs" rel="external" title="Vers Facebook" class="social__link social__link--facebook">
                     {!! file_get_contents(asset('svg/facebook.svg')) !!}
                     <span>Vers Facebook</span>
                 </a>
             </li>
             <li class="social__item">
-                <a href="" itemprop="sameAs" rel="external" title="Vers Twitter" class="social__link social__link--twitter">
+                <a href="{{ Config::get('settings.social_tw') }}" itemprop="sameAs" rel="external" title="Vers Twitter" class="social__link social__link--twitter">
                     {!! file_get_contents(asset('svg/twitter.svg')) !!}
                     <span>Vers Twitter</span>
                 </a>
             </li>
             <li class="social__item">
-                <a href="" itemprop="sameAs" rel="external" title="Vers Pinterest" class="social__link social__link--pinterest">
+                <a href="{{ Config::get('settings.social_pinterest') }}" itemprop="sameAs" rel="external" title="Vers Pinterest" class="social__link social__link--pinterest">
                     {!! file_get_contents(asset('svg/pinterest.svg')) !!}
                     <span>Vers Pinterest</span>
                 </a>
             </li>
             <li class="social__item">
-                <a href="" itemprop="sameAs" rel="external" title="Vers Behance" class="social__link social__link--behance">
+                <a href="{{ Config::get('settings.social_behance') }}" itemprop="sameAs" rel="external" title="Vers Behance" class="social__link social__link--behance">
                     {!! file_get_contents(asset('svg/behance.svg')) !!}
                     <span>Vers Behance</span>
                 </a>
             </li>
             <li class="social__item">
-                <a href="" itemprop="sameAs" rel="external" title="Vers Dribble" class="social__link social__link--dribble">
+                <a href="{{ Config::get('settings.social_dribble') }}" itemprop="sameAs" rel="external" title="Vers Dribble" class="social__link social__link--dribble">
                     {!! file_get_contents(asset('svg/dribble.svg')) !!}
                     <span>Vers Dribble</span>
                 </a>
@@ -40,12 +40,12 @@
     <section class="section section--contact">
         <h3 role="heading" aria-level="3" class="section__title">Contact</h3>
         <address class="section__address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-            <span itemprop="streetAddress">Rue Peetermans, 80</span>
-            <span><span itemprop="postalCode">4100</span> - <span itemprop="addressLocality">Seraing</span></span>
-            <span itemprop="addressCountry">Belgique</span>
+            <span itemprop="streetAddress">{{ Config::get('settings.address_street') }}, {{ Config::get('settings.address_num') }}</span>
+            <span><span itemprop="postalCode">{{ Config::get('settings.address_cp') }} </span> - <span itemprop="addressLocality">{{ Config::get('settings.address_town') }}</span></span>
+            <span itemprop="addressCountry">{{ Config::get('settings.address_country') }}</span>
         </address>
         <meta itemprop="name" content="Haute École de la Province de Liège - Infographie" />
-        <a href="tel:085211113" class="section__tel" itemprop="telephone">085 21 11 13</a>
+        <a href="tel:{{ str_replace(' ', '', Config::get('settings.contact_tel')) }}" class="section__tel" itemprop="telephone">{{ Config::get('settings.contact_tel') }}</a>
         <a href="{{ Route('contact') }}" class="section__contact">@lang('footer.contact-us')</a>
     </section>
     <section class="section section--explorez">
@@ -72,10 +72,10 @@
                 <a href="{{ Route('webTraining') }}" class="section__link">@lang('footer.webtraining')</a>
             </li>
             <li class="section__item">
-                <a href="#web" class="section__link">@lang('footer.2Dtraining')</a>
+                <a href="{{ Route('webTraining') }}" class="section__link">@lang('footer.2Dtraining')</a>
             </li>
             <li class="section__item">
-                <a href="#web" class="section__link">@lang('footer.3Dtraining')</a>
+                <a href="{{ Route('webTraining') }}" class="section__link">@lang('footer.3Dtraining')</a>
             </li>
         </ul>
         <a href="{{ Route('registration') }}" class="section__cta">@lang('footer.inscription')</a>
