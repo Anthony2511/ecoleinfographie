4b621648db8b5d366f6657e8f8aea17f1c7790cb
Jimmy <jimmy@letecheur.me>
2017-06-01 01:01:42 +0200

    Create Services/Trending for get 5 el most viewe post

---
+++ ecoleinfographie/app/Services/Trending.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use Analytics;
+use Spatie\Analytics\Period;
+
+class Trending
+{
+    public function week()
+    {
+        return $this->getResults($limit = 5);
+    }
+    
+    protected function getResults($days, $limit = 5)
+    {
+        $data = Analytics::fetchMostVisitedPages(Period::days($days), $limit + 10);
+        return $this->parseResults($data, $limit);
+    }
+    
+    protected function parseResults($data, $limit)
+    {
+        return $data->reject(function ($item){
+            return !starts_with($item['url'], '/blog') or
+                $item['url'] == '/blog' or
+                str_contains($item['url'], ['?page', '?category', '&category', '?tag', '&tag']);
+        })->unique('url')->transform(function ($item){
+            $item['pageTitle'] = str_replace(' | École d’infographie de la Province de Liège', '', $item['pageTitle']);
+            return $item;
+        })->splice(0, $limit);
+    }
+}
